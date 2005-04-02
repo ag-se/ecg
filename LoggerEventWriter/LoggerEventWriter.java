a16eb3ee8e52bee409bdb206141effb9b2281591
its7oas7er <its7oas7er@3537aeea-94f2-0310-a3c2-ed66167d6be7>
2005-04-02 21:40:56 +0000

    git-svn-id: svn://svn.berlios.de/ecg/trunk@39 3537aeea-94f2-0310-a3c2-ed66167d6be7

---
+++ LoggerEventWriter/LoggerEventWriter.java
@@ -0,0 +1,65 @@
+
+/*
+ * Created on 07.03.2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+import org.electrocodeogram.module.writer.EventWriter;
+import org.electrocodeogram.sensorwrapper.EventPacket;
+
+
+/**
+ * @author 7oas7er
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class LoggerEventWriter extends EventWriter
+{
+    /**
+     * @param name
+     */
+    public LoggerEventWriter()
+    {
+        super("LoggerEventWriter");
+        // TODO Auto-generated constructor stub
+    }
+
+
+    Logger logger = Logger.getAnonymousLogger();
+
+   
+    /* (non-Javadoc)
+     * @see net.datenfabrik.microstat.core.writer.EventWriter#write(java.lang.String)
+     */
+    public void write(EventPacket eventPacket)
+    {
+        String eventString = new String(eventPacket.getTimeStamp().toString() + " : " + eventPacket.getCommandName());
+        
+        List argList = eventPacket.getArglist();
+        
+        if (argList != null)
+        {
+	            
+            Object[] args = eventPacket.getArglist().toArray();
+        
+            for(int i = 0; i < args.length; i++)
+	        {
+	            String str = (String) args[i];
+	            
+	            eventString += str;
+	        }
+        }
+        
+        eventString += "\n";
+        
+        logger.log(Level.INFO, eventString);
+    }
+
+}
