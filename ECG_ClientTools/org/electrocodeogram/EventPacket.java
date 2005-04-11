44518b18ac76ec756a770c5b2d3a728515936e7b
its7oas7er <its7oas7er@3537aeea-94f2-0310-a3c2-ed66167d6be7>
2005-04-11 11:46:35 +0000

    git-svn-id: svn://svn.berlios.de/ecg/trunk@66 3537aeea-94f2-0310-a3c2-ed66167d6be7

--- ECG_ClientTools/org/electrocodeogram/EventPacket.java
+++ ECG_ClientTools/org/electrocodeogram/EventPacket.java
@@ -6,8 +6,6 @@ import java.util.List;
 /*
  * Created on 12.03.2005
  *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
 
 /**
