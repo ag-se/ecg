d802ca5943a5aa278fa9982008d202e93bcb72d1
its7oas7er <its7oas7er@3537aeea-94f2-0310-a3c2-ed66167d6be7>
2005-05-02 08:55:45 +0000

    git-svn-id: svn://svn.berlios.de/ecg/trunk@72 3537aeea-94f2-0310-a3c2-ed66167d6be7

--- ECG_ClientTools/org/electrocodeogram/EventPacket.java
+++ ECG_ClientTools/org/electrocodeogram/EventPacket.java
@@ -17,13 +17,48 @@ import java.util.List;
 public class EventPacket implements Serializable
 {
 
-    //private String eventString = null;
+    public static final String ECG_TYPE_PREFIX = "ECG_TYPE:";
     
+    public static final String ECG_TYPE_WINDOW_DEACTIVATED = "Window deactivated";
+    
+    public static final String ECG_TYPE_WINDOW_ACTIVATED = "Window activated";
+    
+    public static final String ECG_TYPE_WINDOW_OPENED = "Window opened";
+    
+    public static final String ECG_TYPE_EDITOR_ACTIVATED = "Editor activated";
+    
+    public static final String ECG_TYPE_EDITOR_DEACTIVATED = "Editor deactivated";
+    
+    public static final String ECG_TYPE_PART_ACTIVATED = "Part activated";
+    
+    public static final String ECG_TYPE_PART_DEACTIVATED = "Part deactivated";
+    
+    public static final String ECG_TYPE_EDITOR_OPENED = "Editor opened";
+    
+    public static final String ECG_TYPE_EDITOR_CLOSED = "Editor closed";
+    
+    public static final String ECG_TYPE_PART_OPENED = "Part opened";
+    
+    public static final String ECG_TYPE_PART_CLOSED = "Part closed";
+    
+    public static final String ECG_TYPE_CODECHANGE = "Codechange";
+    
+    public static final String ECG_TYPE_OPEN_FILE = "File opened";
+    
+    public static final String ECG_TYPE_BREAKPOINT_SET = "Breakpoint set";
+    
+    public static final String ECG_TYPE_BREAKPOINT_UNSET = "Breakpoint unset";
+    
+    public static final String ECG_TYPE_RUN = "Run";
+    
+    public static final String ECG_TYPE_DEBUG = "Debug";
+
+        
     private int eventSourceId = -1;
 
     private Date timeStamp = null;
     
-    private String commandName = null;
+    private String hsCommandName = null;
     
     private List argList = null;
     
@@ -34,14 +69,14 @@ public class EventPacket implements Serializable
      * @param commandName
      * @param argList
      */
-    public EventPacket(int id, Date timeStamp, String commandName, List argList)
+    public EventPacket(int id, Date timeStamp, String hsCommandName, List argList)
     {
         
         this.eventSourceId = id;
         
         this.timeStamp = timeStamp;
         
-        this.commandName = commandName;
+        this.hsCommandName = hsCommandName;
         
         this.argList = argList;    
     }
@@ -66,10 +101,24 @@ public class EventPacket implements Serializable
      * 
      * @uml.property name="commandName"
      */
-    public String getCommandName() {
-        return commandName;
+    public String getHsCommandName() {
+        return hsCommandName;
     }
 
+    public String getEcgCommandName()
+    {
+        for(int i=0;i<argList.size();i++)
+        {
+            String s = (String) argList.get(i);
+            
+            if(s.startsWith(ECG_TYPE_PREFIX))
+            {
+                return s.substring(ECG_TYPE_PREFIX.length());
+            }
+        }
+        
+        return null;
+    }
     
     public List getArglist()
     {
