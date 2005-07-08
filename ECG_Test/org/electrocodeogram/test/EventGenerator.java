7576748d42db569772c191940195d0c8e62a4e57
its7oas7er <its7oas7er@3537aeea-94f2-0310-a3c2-ed66167d6be7>
2005-07-08 12:20:02 +0000

    git-svn-id: svn://svn.berlios.de/ecg/trunk@137 3537aeea-94f2-0310-a3c2-ed66167d6be7

--- ECG_Test/org/electrocodeogram/test/EventGenerator.java
+++ ECG_Test/org/electrocodeogram/test/EventGenerator.java
@@ -1,5 +1,11 @@
 package org.electrocodeogram.test;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.LinkedList;
@@ -17,12 +23,50 @@ import org.electrocodeogram.event.ValidEventPacket;
 public class EventGenerator
 {
 
+    private final String filename = "pseudorandom.strings";
+
+    private BufferedReader br = null;
+
+    private int lineCount = 100;
+
+    private int lineLength = 100;
+
+    private String[] randomStrings = new String[this.lineLength];
+
     /**
      * This enum collects represents all declared HackyStat SensorDataTypes.
      */
-    public enum SensorDataType {ACTIVITY, BUFFTRANS, BUILD, CLI, COMMIT, COVERAGE, DEPENDENCY, FILEMETRIC, ISSUE, PERF, REVIEWACTIVITY, REVIEWISSUE, UNITTEST};
+    public enum SensorDataType {
+        ACTIVITY, BUFFTRANS, BUILD, CLI, COMMIT, COVERAGE, DEPENDENCY, FILEMETRIC, ISSUE, PERF, REVIEWACTIVITY, REVIEWISSUE, UNITTEST
+    };
 
-   
+    /**
+     * This creates the EventGenerator and initializes the randomString Array
+     * by reading in the "pseudorandom.strings" file.
+     * @throws IOException If initialization of the Reader or the reading of the file fails
+     */
+    public EventGenerator() throws IOException
+    {
+
+        File file = new File(
+                ".." + File.separator + "ECG_Test" + File.separator + this.filename);
+
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+
+        this.br = new BufferedReader(new FileReader(file));
+
+        int i = 0;
+
+        while (i < this.lineCount && (this.randomStrings[i++] = this.br.readLine()) != null) {
+            // do nothing  
+        }
+
+        this.br.close();
+
+    }
+
     // create a syntactically valid or invalid Date
     private Date createDate(boolean syntValidDatePar)
     {
@@ -35,18 +79,22 @@ public class EventGenerator
     }
 
     // create a syntactically valid or invalid commandName
-    private String createCommandName(boolean syntValidCommandName)
+    private String createCommandName(boolean syntValidCommandName, int line) throws NoTestDataException
     {
-        if (syntValidCommandName) {
-            return createRandomString(20);
+        if (!syntValidCommandName) {
+            return null;
         }
 
-        return null;
+        if (line < 0 || line > this.lineCount) {
+            throw new NoTestDataException();
+        }
+
+        return this.randomStrings[line];
 
     }
 
     //  create a syntactically valid or invalid argList of given size
-    private List createArgList(boolean argListNotNull, boolean argListOfString, int argListLength, int argListEntrySize)
+    private List createArgList(boolean argListNotNull, boolean argListOfString, int argListLength, int argListEntrySize) throws NoTestDataException
     {
         if (!argListNotNull) {
             return null;
@@ -69,6 +117,7 @@ public class EventGenerator
      *            Shall the timestamp be syntacticallly vaild?
      * @param syntValidCommandName
      *            Shall the commmandName be syntacticallly vaild?
+     * @param line Is the linenumber of the String to use as the commandName from the "pseudorandom.strings" file
      * @param argListNotNull
      *            Shall the argList be not null?
      * @param argListOfString
@@ -78,14 +127,15 @@ public class EventGenerator
      * @param argListEntrySize
      *            The size of each list element
      * @return An EventPacket of the desired kind
+     * @throws NoTestDataException If a pseudorandom String is requested by a line number that is not available or if the requested String size is to higher then available
      */
-    public EventPacket createEventPacket(boolean syntValidDate, boolean syntValidCommandName, boolean argListNotNull, boolean argListOfString, int argListLength, int argListEntrySize)
+    public EventPacket createEventPacket(boolean syntValidDate, boolean syntValidCommandName, int line, boolean argListNotNull, boolean argListOfString, int argListLength, int argListEntrySize) throws NoTestDataException
     {
         EventPacket eventPacket = null;
         eventPacket = new EventPacket(
                 0,
                 createDate(syntValidDate),
-                createCommandName(syntValidCommandName),
+                createCommandName(syntValidCommandName, line),
                 createArgList(argListNotNull, argListOfString, argListLength, argListEntrySize));
 
         return eventPacket;
@@ -103,6 +153,7 @@ public class EventGenerator
      *            Shall the timestamp be syntacticallly vaild?
      * @param syntValidCommandName
      *            Shall the commmandName be syntacticallly vaild?
+     * @param line Is the linenumber of the String to use as the commandName from the "pseudorandom.strings" file
      * @param argListNotNull
      *            Shall the argList be not null?
      * @param argListOfString
@@ -113,90 +164,86 @@ public class EventGenerator
      *            The size of each list element
      * @return An EventPacket of the desired kind
      * @throws IllegalEventParameterException This is thrown if the passes parameter data does not conform to the syntax rules of event data.
+     * @throws NoTestDataException If a pseudorandom String is requested by a line number that is not available or if the requested String size is to higher then available
      */
-    public ValidEventPacket createValidEventPacket(boolean syntValidDate, boolean syntValidCommandName, boolean argListNotNull, boolean argListOfString, int argListLength, int argListEntrySize) throws IllegalEventParameterException
+    public ValidEventPacket createValidEventPacket(boolean syntValidDate, boolean syntValidCommandName, int line, boolean argListNotNull, boolean argListOfString, int argListLength, int argListEntrySize) throws IllegalEventParameterException, NoTestDataException
     {
         ValidEventPacket eventPacket = null;
         eventPacket = new ValidEventPacket(
                 0,
                 createDate(syntValidDate),
-                createCommandName(syntValidCommandName),
+                createCommandName(syntValidCommandName, line),
                 createArgList(argListNotNull, argListOfString, argListLength, argListEntrySize));
 
         return eventPacket;
     }
-    
+
     /**
      * This method creates and returns a valid HackyStat SensorDataType event.
      * @param type Is the SensorDataType to create
+     * @param line Is the linenumber of the String to use as the commandName from the "pseudorandom.strings" file
      * @return A valid HackyStat event.
+     * @throws NoTestDataException If a pseudorandom String is requested by a line number that is not available or if the requested String size is to higher then available
      */
-    public ValidEventPacket createHackyStatEventPacket(SensorDataType type)
+    public ValidEventPacket createHackyStatEventPacket(SensorDataType type, int line) throws NoTestDataException
     {
-        ValidEventPacket eventPacket = null;
-     
-        String[] args = null;
         
+        if (line < 0 || line > this.lineCount) {
+            throw new NoTestDataException();
+        }
+        
+        ValidEventPacket eventPacket = null;
+
+        String[] args = null;
+
         switch (type)
         {
         case ACTIVITY:
-            
-            args = new String[]{"add","test"};
-            
+
+            args = new String[] { "add", this.randomStrings[line]};
+
             try {
-                eventPacket = new ValidEventPacket(
-                        0,
-                        createDate(true),
-                        "Activity",
-                        Arrays.asList(args));
+                eventPacket = new ValidEventPacket(0, createDate(true),
+                        "Activity", Arrays.asList(args));
             }
             catch (IllegalEventParameterException e) {
                 e.printStackTrace();
             }
-            
+
             break;
 
         case BUFFTRANS:
-            
-            args = new String[]{"add","test","test","test"};
-            
+
+            args = new String[] { "add", this.randomStrings[line], this.randomStrings[line], this.randomStrings[line]};
+
             try {
-                eventPacket = new ValidEventPacket(
-                        0,
-                        createDate(true),
-                        "BuffTrans",
-                        Arrays.asList(args));
+                eventPacket = new ValidEventPacket(0, createDate(true),
+                        "BuffTrans", Arrays.asList(args));
             }
             catch (IllegalEventParameterException e) {
                 e.printStackTrace();
             }
-       
+
         case BUILD:
-            
-            args = new String[]{"add","test","test","test","test","test","test","test","test","test","test"};
-            
+
+            args = new String[] { "add", this.randomStrings[line], this.randomStrings[line], this.randomStrings[line], this.randomStrings[line], this.randomStrings[line], this.randomStrings[line], this.randomStrings[line], this.randomStrings[line], this.randomStrings[line], this.randomStrings[line] };
+
             try {
-                eventPacket = new ValidEventPacket(
-                        0,
-                        createDate(true),
-                        "Build",
-                        Arrays.asList(args));
+                eventPacket = new ValidEventPacket(0, createDate(true),
+                        "Build", Arrays.asList(args));
             }
             catch (IllegalEventParameterException e) {
                 e.printStackTrace();
             }
-            
+
             break;
-            
+
         case CLI:
-            
-            args = new String[]{"add","test","test","test","test"};
-            
+
+            args = new String[] { "add", this.randomStrings[line], this.randomStrings[line], this.randomStrings[line], this.randomStrings[line] };
+
             try {
-                eventPacket = new ValidEventPacket(
-                        0,
-                        createDate(true),
-                        "CLI",
+                eventPacket = new ValidEventPacket(0, createDate(true), "CLI",
                         Arrays.asList(args));
             }
             catch (IllegalEventParameterException e) {
@@ -204,62 +251,53 @@ public class EventGenerator
             }
 
         case COMMIT:
-            
-            args = new String[]{"add","test","test","test","test","test","test","1","1","1","test"};
-            
+
+            args = new String[] { "add", this.randomStrings[line], this.randomStrings[line], this.randomStrings[line], this.randomStrings[line], this.randomStrings[line], this.randomStrings[line], "" + line, "" + line, "" + line, this.randomStrings[line] };
+
             try {
-                eventPacket = new ValidEventPacket(
-                        0,
-                        createDate(true),
-                        "Commit",
-                        Arrays.asList(args));
+                eventPacket = new ValidEventPacket(0, createDate(true),
+                        "Commit", Arrays.asList(args));
             }
             catch (IllegalEventParameterException e) {
                 e.printStackTrace();
             }
-            
+
             break;
-            
+
         case FILEMETRIC:
-            
-            args = new String[]{"addFileMetric","bcml","C:\\cvs\\foobarproject\\src\foo\\bar\\Bar.java","foo.bar.Bar","cbo=1,loc=2","1049798488530"};
-            
+
+            args = new String[] { "addFileMetric", "bcml", "C:\\cvs\\foobarproject\\src\foo\\bar\\Bar.java", "foo.bar.Bar", "cbo=1,loc=2", "1049798488530" };
+
             try {
-                eventPacket = new ValidEventPacket(
-                        0,
-                        createDate(true),
-                        "FileMetric",
-                        Arrays.asList(args));
+                eventPacket = new ValidEventPacket(0, createDate(true),
+                        "FileMetric", Arrays.asList(args));
             }
             catch (IllegalEventParameterException e) {
                 e.printStackTrace();
             }
-            
+
             break;
-          
+
         case UNITTEST:
-            
-            args = new String[]{"add","test","test","3000","test","test"};
-            
+
+            args = new String[] { "add", this.randomStrings[line], this.randomStrings[line], "3000", this.randomStrings[line], this.randomStrings[line] };
+
             try {
-                eventPacket = new ValidEventPacket(
-                        0,
-                        createDate(true),
-                        "UnitTest",
-                        Arrays.asList(args));
+                eventPacket = new ValidEventPacket(0, createDate(true),
+                        "UnitTest", Arrays.asList(args));
             }
             catch (IllegalEventParameterException e) {
                 e.printStackTrace();
             }
-            
+
             break;
-            
+
         default:
             break;
         }
         return eventPacket;
     }
-    
+
     /**
      * Given a String[] this method returns a syntactically valid List of it
      * with "add" and "testdata" being the first to entries.
@@ -290,7 +328,8 @@ public class EventGenerator
     }
 
     /**
-     * This methods creates a String[] of random payload.
+     * This methods creates a String[] of pseudorandom payload
+     * by reading in the file "pseudorandom.strings".
      * 
      * @param arraySize
      *            The size of the String[]
@@ -298,13 +337,27 @@ public class EventGenerator
      *            The size of each String element
      * @return
      *            An Arrray of random Strings
+     * @throws NoTestDataException If a pseudorandom String is requested by a line number that is not available or if the requested String size is to higher then available 
      */
-    private String[] createPayloadStringArray(int arraySize, int stringSize)
+    private String[] createPayloadStringArray(int arraySize, int stringSize) throws NoTestDataException
     {
+
+        if (arraySize < 0 || arraySize > this.lineCount) {
+            throw new NoTestDataException();
+        }
+
+        if (stringSize < 0 || stringSize > this.lineLength) {
+            throw new NoTestDataException();
+        }
+
         String[] payloadStringArray = new String[arraySize];
 
         for (int i = 0; i < arraySize; i++) {
-            payloadStringArray[i] = new String(createRandomString(stringSize));
+
+            String randomString = this.randomStrings[i];
+
+            payloadStringArray[i] = randomString.substring(0, stringSize);
+
         }
 
         return payloadStringArray;
@@ -312,7 +365,7 @@ public class EventGenerator
 
     /**
      * This methods creates and returns a random String of the given element
-     * count.
+     * count. It is used to generate the file "pseudorandom.strings".
      * 
      * @param stringSize
      *            The number of elements in the String to create randomly
@@ -339,5 +392,39 @@ public class EventGenerator
 
         return string;
     }
-    
+
+    /**
+     * Calling this method causes it to generate a file with randomized
+     * Strings to be used in the testcases through the ECG framework testing.
+     * @throws IOException If writing the file fails
+     */
+    public void generateRandomStringFile() throws IOException
+    {
+        BufferedWriter bw = new BufferedWriter(new FileWriter(this.filename));
+
+        for (int i = 0; i < this.lineCount; i++) {
+            bw.write(this.createRandomString(this.lineLength) + "\n");
+        }
+
+        bw.close();
+    }
+
+    /**
+     * When the main method is called, the file "pseudorandom.strings" is (re)created.
+     * 
+     * @param args Is not used
+     */
+    public static void main(String args[])
+    {
+        try {
+            EventGenerator eventGenerator = new EventGenerator();
+
+            eventGenerator.generateRandomStringFile();
+        }
+        catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+    }
 }
