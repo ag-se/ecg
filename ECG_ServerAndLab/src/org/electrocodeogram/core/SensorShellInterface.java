26e0abd51c402e563b2d984db78b5314602ac939
its7oas7er <its7oas7er@3537aeea-94f2-0310-a3c2-ed66167d6be7>
2005-07-20 08:29:00 +0000

    git-svn-id: svn://svn.berlios.de/ecg/trunk@198 3537aeea-94f2-0310-a3c2-ed66167d6be7

--- ECG_ServerAndLab/src/org/electrocodeogram/core/SensorShellInterface.java
+++
@@ -1,9 +0,0 @@
-package org.electrocodeogram.core;
-
-import java.util.Date;
-import java.util.List;
-
-public interface SensorShellInterface
-{
-    public boolean doCommand(Date timeStamp, String commandName, List argList);
-}
