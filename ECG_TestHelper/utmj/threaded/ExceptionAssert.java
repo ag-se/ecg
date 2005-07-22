a996d27550821f458dac040943ca23df0e2841ff
its7oas7er <its7oas7er@3537aeea-94f2-0310-a3c2-ed66167d6be7>
2005-07-22 09:20:43 +0000

    git-svn-id: svn://svn.berlios.de/ecg/trunk@236 3537aeea-94f2-0310-a3c2-ed66167d6be7

---
+++ ECG_TestHelper/utmj/threaded/ExceptionAssert.java
@@ -0,0 +1,78 @@
+package utmj.threaded;
+
+import java.util.*;
+import junit.framework.*;
+
+/** 
+ * Copyright (c) 2001, <a href="mailto:john.link@gmx.net">Johannes Link</a>
+ *
+ * Assert that an uncaught exception is being thrown in main thread or sub threads.
+ */
+public abstract class ExceptionAssert {
+	private Class exceptionType;
+	private long millisecondsToTimeout;
+	private Throwable caughtException = null;
+
+public ExceptionAssert(Class exceptionType, long millisecondsToTimeout) {
+	this.exceptionType = exceptionType;
+	this.millisecondsToTimeout = millisecondsToTimeout;
+}
+
+
+public Throwable getCaughtException() {
+	return caughtException;
+}
+
+
+private void handleCaughtException(Throwable ex) {
+	if (caughtException == null) {
+		setCaughtException(ex);
+	}
+}
+
+
+public abstract void run();
+
+
+private synchronized void setCaughtException(Throwable ex) {
+	caughtException = ex;
+	this.notify();
+}
+
+
+public void start() throws AssertionFailedError {
+	ThreadGroup group = new ThreadGroup("exception assert") {
+		public void uncaughtException(Thread t, Throwable ex) {
+			handleCaughtException(ex);
+		}
+	};
+	new Thread(group, "test thread") {
+		public void run() {
+			ExceptionAssert.this.run();
+		}
+	}
+	.start();
+	this.waitForException();
+	if (caughtException == null) {
+		Assert.fail("No Exception thrown");
+	}
+	if (!exceptionType.isAssignableFrom(caughtException.getClass())) {
+		Assert.fail(
+			"Expected "
+				+ exceptionType.getName()
+				+ " but caught "
+				+ caughtException.toString());
+	}
+
+}
+
+
+private synchronized void waitForException() {
+	try {
+		if (caughtException == null) {
+			this.wait(millisecondsToTimeout);
+		}
+	} catch (InterruptedException ignore) {
+	}
+}
+}
