84a6bbd1fcc6b9666792bf7f3ee99e5a45c39f18
its7oas7er <its7oas7er@3537aeea-94f2-0310-a3c2-ed66167d6be7>
2005-07-22 09:25:18 +0000

    git-svn-id: svn://svn.berlios.de/ecg/trunk@242 3537aeea-94f2-0310-a3c2-ed66167d6be7

---
+++ ECG_SensorShellTests/org/electrocodeogram/test/client/mocksensor/MockSensor.java
@@ -0,0 +1,63 @@
+package org.electrocodeogram.test.client.mocksensor;
+
+import java.util.Date;
+
+import org.electrocodeogram.event.EventPacket;
+import org.hackystat.kernel.admin.SensorProperties;
+import org.hackystat.kernel.shell.SensorShell;
+
+/**
+ * This is the ECG TestSensor used for automated JUnit tests. It is capable of
+ * generating valid and different kinds of invalid EventPackets and it defines
+ * methods to send these EventPackets.
+ */
+public class MockSensor
+{
+
+    private SensorShell shell = null;
+
+    private SensorProperties properties = null;
+    
+    protected Date sendingTime = null;
+    
+    
+
+    /**
+     * Creates a TestSensor instance and initializes it with a SensorShell.
+     *
+     */
+    public MockSensor()
+    {
+        this.properties = new SensorProperties("TestSensor");
+
+        this.shell = new SensorShell(this.properties, false, "TestSensor");
+    }
+
+   
+    
+    /**
+     * This method passes a single given EventPacket to the ECG SensorShell.
+     * 
+     * @param eventPacket
+     *            The EventPacket to pass
+     * @return The result as given by the ECG Sensorshell. "true" means the
+     *         EventPacket is syntactically valid and accepted. "false" means
+     *         the eventPacket is syntactically invalid and not acccepted.
+     */
+    public boolean sendEvent(EventPacket eventPacket)
+    {
+        this.sendingTime = new Date();
+        
+        return this.shell.doCommand(eventPacket.getTimeStamp(), eventPacket.getHsCommandName(), eventPacket.getArglist());
+    }
+
+    public Date getSendingTime()
+    {
+        Date toReturn = this.sendingTime;
+        
+        this.sendingTime = null;
+        
+        return toReturn;
+    }
+
+}
