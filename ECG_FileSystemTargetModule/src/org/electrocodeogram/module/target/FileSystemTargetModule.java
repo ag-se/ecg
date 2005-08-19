1e2168b218a32b634d506e31eb6c8485376d8426
its7oas7er <its7oas7er@3537aeea-94f2-0310-a3c2-ed66167d6be7>
2005-08-19 15:13:12 +0000

    git-svn-id: svn://svn.berlios.de/ecg/trunk@363 3537aeea-94f2-0310-a3c2-ed66167d6be7

--- ECG_FileSystemTargetModule/src/org/electrocodeogram/module/target/FileSystemTargetModule.java
+++ ECG_FileSystemTargetModule/src/org/electrocodeogram/module/target/FileSystemTargetModule.java
@@ -1,4 +1,5 @@
 package org.electrocodeogram.module.target;
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -6,6 +7,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import org.electrocodeogram.event.ValidEventPacket;
+import org.electrocodeogram.module.ModulePropertyException;
 import org.electrocodeogram.module.target.TargetModule;
 
 /**
@@ -15,11 +17,11 @@ public class FileSystemTargetModule extends TargetModule
 {
 
     private String outputFileName = null;
-    
+
     private File outputFile = null;
-    
+
     private PrintWriter writer = null;
-        
+
     /**
      * @param arg0
      * @param arg1
@@ -27,21 +29,22 @@ public class FileSystemTargetModule extends TargetModule
     public FileSystemTargetModule(int arg0, String arg1)
     {
         super(arg0, arg1);
-        
+
         String homeDir = System.getProperty("user.home");
-        
-        if(homeDir == null)
-        {
+
+        if (homeDir == null) {
             homeDir = ".";
         }
-        
+
         this.outputFileName = "test.txt";
-        
-        this.outputFile = new File(homeDir + File.separator + this.outputFileName);
-        
+
+        this.outputFile = new File(
+                homeDir + File.separator + this.outputFileName);
+
         try {
-            this.writer = new PrintWriter(new BufferedWriter(new FileWriter(this.outputFile,true)));
-            
+            this.writer = new PrintWriter(new BufferedWriter(new FileWriter(
+                    this.outputFile, true)));
+
         }
         catch (IOException e) {
             // TODO Auto-generated catch block
@@ -55,21 +58,46 @@ public class FileSystemTargetModule extends TargetModule
     @Override
     public void write(ValidEventPacket arg0)
     {
-        
+
         this.writer.println(arg0.toString());
-        
+
         this.writer.flush();
-        
+
     }
 
     /**
+     * @throws ModulePropertyException 
      * 
      */
-    public void setProperty(String propertyName, Object propertyValue)
+    public void setProperty(String propertyName, Object propertyValue) throws ModulePropertyException
     {
-        
+        if (!propertyName.equals("Output File")) {
+            System.out.println("A");
+
+            return;
+        }
+
+        if (!(propertyValue instanceof File)) {
+            System.out.println("B");
+
+            return;
+        }
+
+        File propertyValueFile = (File) propertyValue;
+
+        this.outputFile = propertyValueFile;
+
+        this.writer.close();
+
+        try {
+            this.writer = new PrintWriter(new FileWriter(this.outputFile));
+        }
+        catch (IOException e) {
+
+            System.out.println("C");
+            throw new ModulePropertyException(
+                    "The file could not be opened for writing.");
+        }
+
     }
-
-    
-
 }
