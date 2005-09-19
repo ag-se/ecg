0383fcdd84c3236f49f523dcd6ed22eb7062d7fc
its7oas7er <its7oas7er@3537aeea-94f2-0310-a3c2-ed66167d6be7>
2005-09-19 12:14:55 +0000

    git-svn-id: svn://svn.berlios.de/ecg/trunk@432 3537aeea-94f2-0310-a3c2-ed66167d6be7

--- ECG_SocketSourceModule/src/org/electrocodeogram/module/source/SocketSourceModule.java
+++ ECG_SocketSourceModule/src/org/electrocodeogram/module/source/SocketSourceModule.java
@@ -1,103 +1,110 @@
 package org.electrocodeogram.module.source;
 
-import java.util.Observable;
-
 import org.electrocodeogram.module.ModulePropertyException;
-    /**
-     * This module receives event data from multiple client sensors.
-     * The communication is done over Sockets. Each new incoming
-     * communication request starts a new SocketServerThread which
-     * than receives the event data from the new client.
-     *
-     */
-    public class SocketSourceModule extends SourceModule
-    {
 
-        private int port;
-        
-        private SocketServer socketServer;
-        
-        /**
-         * @param moduleClassId
-         * @param name
-         */
-        public SocketSourceModule(String moduleClassId, String name)
-        {
-            super(moduleClassId, name);
-            
-        }
+/**
+ * This module receives event data from multiple client sensors.
+ * The communication is done over Sockets. Each new incoming
+ * communication request starts a new SocketServerThread which
+ * than receives the event data from the new client.
+ *
+ */
+public class SocketSourceModule extends SourceModule
+{
 
+	private int port;
 
-        /**
-         * @see org.electrocodeogram.module.source.SourceModule#startReader(org.electrocodeogram.module.source.SourceModule)
-         */
-        @Override
-        public void startReader(SourceModule sourceModule)
-        {
-            this.socketServer = new SocketServer(sourceModule,this.port);
-            
-            this.socketServer.start();
-            
-        }
+	private SocketServer socketServer;
 
-       
-        /**
-         * @throws ModulePropertyException 
-         * @see org.electrocodeogram.module.Module#setProperty(java.lang.String, java.lang.Object)
-         */
-        @Override
-        public void setProperty(String propertyName, Object propertyValue) throws ModulePropertyException
-        {
-            if(propertyName.equals("port"))
-            {
-                if(propertyValue instanceof String)
-                {
-                    String propertyValueString = (String) propertyValue;
-                    
-                    try
-                    {
-                        int portValue = Integer.parseInt(propertyValueString);
-                        
-                        if(portValue > 1024 && portValue < 65536)
-                        {
-                            this.port = portValue;
-                            
-                            this.socketServer.shutDown();
-                            
-                            this.startReader(this);
-                            
-                        }
-                        
-                        throw new ModulePropertyException("The value for the port property must be a number greater than 1024 and less then 65536.");
-                         
-                    }
-                    catch(NumberFormatException e)
-                    {
-                        throw new ModulePropertyException("The value for the port property must be a number greater than 1024 and less then 65536.");
-                    }
-                }
-                
-                throw new ModulePropertyException("The value for the port property must be a number greater than 1024 and less then 65536.");
-            }
-        }
+	/**
+	 * @param moduleClassId
+	 * @param name
+	 */
+	public SocketSourceModule(String moduleClassId, String name)
+	{
+		super(moduleClassId, name);
 
+	}
 
-       public void analyseCoreNotification()
-       {
-           
-       }
+	/**
+	 * @see org.electrocodeogram.module.source.SourceModule#startReader(org.electrocodeogram.module.source.SourceModule)
+	 */
+	@Override
+	public void startReader(SourceModule sourceModule)
+	{
+		this.socketServer = new SocketServer(sourceModule, this.port);
 
+		this.socketServer.start();
 
-    /**
-     * @see org.electrocodeogram.module.Module#initialize()
-     */
-    @Override
-    public void initialize()
-    {
-        this.port = 22222;
-        
-        this.startReader(this);
-    }
-    	
-        
-    }
+	}
+
+	/**
+	 * @throws ModulePropertyException 
+	 * @see org.electrocodeogram.module.Module#setProperty(java.lang.String, java.lang.String)
+	 */
+	@Override
+	public void setProperty(String propertyName, String propertyValue) throws ModulePropertyException
+	{
+		if (propertyName.equals("port"))
+		{
+			try
+			{
+				int portValue = Integer.parseInt(propertyValue);
+
+				if (portValue > 1024 && portValue < 65536)
+				{
+					this.port = portValue;
+
+					this.socketServer.shutDown();
+
+					this.startReader(this);
+
+				}
+				else
+				{
+					throw new ModulePropertyException(
+						"The value for the port property must be a number greater than 1024 and less then 65536.");
+				}
+
+			}
+			catch (NumberFormatException e)
+			{
+				throw new ModulePropertyException(
+						"The value for the port property must be a number greater than 1024 and less then 65536.");
+			}
+
+		}
+	}
+
+	public void analyseCoreNotification()
+	{
+
+	}
+
+	/**
+	 * @see org.electrocodeogram.module.Module#initialize()
+	 */
+	@Override
+	public void initialize()
+	{
+		this.port = 22222;
+
+		this.startReader(this);
+
+	}
+
+	/**
+	 * @see org.electrocodeogram.module.Module#getProperty(java.lang.String)
+	 */
+	@Override
+	public String getProperty(String propertyName)
+	{
+		if (propertyName.equals("port"))
+		{
+			return "" + this.port;
+		}
+
+		return null;
+	}
+
+}
