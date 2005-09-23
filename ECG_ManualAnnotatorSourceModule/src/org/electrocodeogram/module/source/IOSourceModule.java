2f6af1a78e7db30c3e5391090a3c48350f0f6a33
its7oas7er <its7oas7er@3537aeea-94f2-0310-a3c2-ed66167d6be7>
2005-09-23 18:58:47 +0000

    git-svn-id: svn://svn.berlios.de/ecg/trunk@452 3537aeea-94f2-0310-a3c2-ed66167d6be7

---
+++ ECG_ManualAnnotatorSourceModule/src/org/electrocodeogram/module/source/IOSourceModule.java
@@ -0,0 +1,229 @@
+package org.electrocodeogram.module.source;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import org.electrocodeogram.event.IllegalEventParameterException;
+import org.electrocodeogram.event.ValidEventPacket;
+import org.electrocodeogram.module.ModulePropertyException;
+
+/**
+ * 
+ */
+public class IOSourceModule extends SourceModule
+{
+
+	private Console _console;
+	
+	/**
+	 * @param arg0
+	 * @param arg1
+	 */
+	public IOSourceModule(String arg0, String arg1)
+	{
+		super(arg0, arg1);
+
+	}
+
+	/**
+	 * @param propertyName
+	 * @param propertyValue
+	 * @throws ModulePropertyException
+	 * 
+	 */
+	@Override
+	public void setProperty(String propertyName, String propertyValue) throws ModulePropertyException
+	{
+
+	}
+
+	public void analyseCoreNotification()
+	{
+
+	}
+
+	/**
+	 * @see org.electrocodeogram.module.Module#initialize()
+	 */
+	@Override
+	public void initialize()
+	{
+		startReader(this);
+	}
+
+	/**
+	 * @see org.electrocodeogram.module.source.SourceModule#startReader(org.electrocodeogram.module.source.SourceModule)
+	 */
+	@Override
+	public void startReader(SourceModule sourceModule)
+	{
+		this._console = new Console(sourceModule);
+
+		this._console.start();
+
+	}
+
+	/**
+	 * @see org.electrocodeogram.module.Module#getProperty(java.lang.String)
+	 */
+	@Override
+	public String getProperty(String propertyName)
+	{
+		return null;
+	}
+
+	private class Console extends Thread
+	{
+
+		private BufferedReader bufferedReader = null;
+
+		private SourceModule _sourceModule;
+
+		private StringTokenizer stringTokenizer;
+
+		/**
+		 * Creates the console to manage the ECG Server & Lab.
+		 * 
+		 */
+		public Console(SourceModule sourceModule)
+		{
+			this._sourceModule = sourceModule;
+
+			this.bufferedReader = new BufferedReader(new InputStreamReader(
+					System.in));
+
+			System.out.println("ElectroCodeoGram Server & Lab is starting...");
+
+		}
+
+		/**
+		 * Here the reading of the console-input is done.
+		 */
+		@Override
+		public void run()
+		{
+			
+			Date timeStamp = null;
+			
+			String eventString;
+			
+			String[] eventStrings;
+			
+			int i;
+			
+			String token;
+			
+			String sensorDataTypeString;
+			
+			String argListString;
+			
+			StringTokenizer argListTokenizer;
+			
+			String[] argListStringArray;
+			
+			ValidEventPacket eventPacket;
+			
+			List argList;
+			
+			while (true)
+			{
+
+				System.out.println(this.getName() + " >>");
+
+				String inputString = "" + this.readLine();
+
+				System.out.println("Echo: " + inputString);
+
+				if (inputString.startsWith("MicroActivity#"))
+				{
+					eventString = inputString.substring(new String(
+							"MicroActivty#").length());
+
+					this.stringTokenizer = new StringTokenizer(eventString,
+							ValidEventPacket.EVENT_SEPARATOR);
+
+					eventStrings = new String[this.stringTokenizer.countTokens()];
+
+					i = 0;
+
+					while (this.stringTokenizer.hasMoreTokens())
+					{
+						token = this.stringTokenizer.nextToken();
+
+						if (token == null || token.equals(""))
+						{
+							break;
+						}
+
+						eventStrings[i++] = token;
+					}
+
+					try
+					{
+						timeStamp = new SimpleDateFormat(
+								ValidEventPacket.DATE_FORMAT_PATTERN).parse(eventStrings[0]);
+					}
+					catch (ParseException e)
+					{
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+
+					sensorDataTypeString = eventStrings[1];
+
+					argListString = eventStrings[2];
+
+					argListTokenizer = new StringTokenizer(
+							argListString, ValidEventPacket.ARGLIST_SEPARATOR);
+
+					argListStringArray = new String[argListTokenizer.countTokens()];
+
+					int j = 0;
+
+					while (argListTokenizer.hasMoreTokens())
+					{
+						argListStringArray[j++] = argListTokenizer.nextToken();
+					}
+
+					argList = Arrays.asList(argListStringArray);
+					
+					eventPacket = null;
+	                
+	                
+                    try
+					{
+						eventPacket = new ValidEventPacket(0,timeStamp,sensorDataTypeString,argList);
+					}
+					catch (IllegalEventParameterException e)
+					{
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+					this._sourceModule.append(eventPacket);
+	                
+				}
+			}
+		}
+
+		private String readLine()
+		{
+			try
+			{
+				return this.bufferedReader.readLine();
+			}
+			catch (IOException e)
+			{
+				return null;
+			}
+		}
+
+	}
+}
---
+++ ECG_ManualAnnotatorSourceModule/src/org/electrocodeogram/module/source/ManualAnnotatorSourceModule.java
@@ -0,0 +1,190 @@
+package org.electrocodeogram.module.source;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.logging.Level;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+import org.electrocodeogram.event.IllegalEventParameterException;
+import org.electrocodeogram.event.ValidEventPacket;
+import org.electrocodeogram.module.ModuleProperty;
+import org.electrocodeogram.module.ModulePropertyException;
+
+/**
+ * 
+ */
+public class ManualAnnotatorSourceModule extends SourceModule
+{
+
+	private ManualAnnotatorFrame _frame;
+	
+	ManualAnnotatorEvents _events;
+	
+	/**
+	 * @param arg0
+	 * @param arg1
+	 */
+	public ManualAnnotatorSourceModule(String arg0, String arg1)
+	{
+		super(arg0, arg1);
+		
+		
+	}
+
+	/**
+	 * @param propertyName
+	 * @param propertyValue
+	 * @throws ModulePropertyException
+	 * 
+	 */
+	@Override
+	public void setProperty(String propertyName, String propertyValue) throws ModulePropertyException
+	{
+		if(propertyName.equals("Show Dialog"))
+		{
+			this._frame.setVisible(true);
+		}
+		else
+		{
+			throw new ModulePropertyException(
+					"The module does not support a property with the given name: " + propertyName);
+		}
+		
+		this.getLogger().log(Level.INFO,"The " + propertyName + " property has been set to " + propertyValue);
+	}
+
+	public void analyseCoreNotification()
+	{
+
+	}
+
+	/**
+	 * @see org.electrocodeogram.module.Module#initialize()
+	 */
+	@Override
+	public void initialize()
+	{
+		for(ModuleProperty property : this.runtimeProperties)
+		{
+			if(property.getName().equals("Events"))
+			{
+				this._events = new ManualAnnotatorEvents(property.getValue());
+			}
+		}
+		
+		startReader(this);
+		
+	}
+
+	/**
+	 * @see org.electrocodeogram.module.source.SourceModule#startReader(org.electrocodeogram.module.source.SourceModule)
+	 */
+	@Override
+	public void startReader(SourceModule sourceModule)
+	{
+		this._frame = new ManualAnnotatorFrame(this);
+	}
+
+	/**
+	 * @see org.electrocodeogram.module.Module#getProperty(java.lang.String)
+	 */
+	@Override
+	public String getProperty(String propertyName)
+	{
+		return null;
+	}
+
+	private static class ManualAnnotatorFrame extends JFrame
+	{
+		
+		private ManualAnnotatorSourceModule _sourceModule;
+		
+		public ManualAnnotatorFrame(ManualAnnotatorSourceModule sourceModule)
+		{
+			this._sourceModule = sourceModule;
+			
+			this.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+			
+			this.setTitle("Manual Annotation");
+			
+			this.setBounds(10,10,200,200);
+			
+			this.setLayout(new FlowLayout());
+			
+			this.setEvents(this._sourceModule._events.getEvents());
+			
+			this.pack();
+			
+			this.setVisible(true);
+				
+		}
+		
+		public void setEvents(String[] events)
+		{
+			for(String event : events)
+			{
+				if(event != null && !event.equals(""))
+				{
+					JButton btnEvent = new JButton(event);
+					
+					btnEvent.addActionListener(new EventActionAdapter(event,this._sourceModule));
+					
+					this.getContentPane().add(btnEvent);
+				}
+			}
+		}
+		
+		private static class EventActionAdapter implements ActionListener
+		{
+
+			private String _event;
+			
+			private ManualAnnotatorSourceModule _sourceModule;
+			
+			public EventActionAdapter(String event, ManualAnnotatorSourceModule sourceModule)
+			{
+				this._event = event;
+				
+				this._sourceModule = sourceModule;
+			}
+			
+			/**
+			 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+			 */
+			public void actionPerformed(ActionEvent e)
+			{
+				String microActivity = "<?xml version=\"1.0\"?><microActivity><manual><value>" + this._event + "</value></manual></microActivity>";
+				
+				String[] args = { "add", "MicroActivity", microActivity};
+				
+				try
+				{
+					ValidEventPacket packet = new ValidEventPacket(this._sourceModule.getId(),new Date(),"Activity",Arrays.asList(args));
+					
+					this._sourceModule.append(packet);
+				}
+				catch (IllegalEventParameterException e1)
+				{
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+			
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see org.electrocodeogram.module.source.SourceModule#stopReader()
+	 */
+	@Override
+	public void stopReader()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+}
