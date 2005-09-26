5376517404a789c66c0168d24d52c578e34f5ac5
its7oas7er <its7oas7er@3537aeea-94f2-0310-a3c2-ed66167d6be7>
2005-09-26 02:49:00 +0000

    git-svn-id: svn://svn.berlios.de/ecg/trunk@477 3537aeea-94f2-0310-a3c2-ed66167d6be7

--- ECG_FileSystemSourceModule/src/org/electrocodeogram/module/source/FileReaderThread.java
+++ ECG_FileSystemSourceModule/src/org/electrocodeogram/module/source/FileReaderThread.java
@@ -18,198 +18,274 @@ import org.electrocodeogram.event.ValidEventPacket;
 import org.electrocodeogram.logging.LogHelper;
 import org.electrocodeogram.module.source.FileSystemSourceModule.ReadMode;
 
-
+/**
+ * This Thread is used by the FileSystemSourceModule to read in
+ * events from a file asynchroneously.
+ *
+ */
 public class FileReaderThread extends Thread
 {
 
 	private Logger _logger;
-	
-	
+
 	private SourceModule _sourceModule;
-	
+
 	private File _inputFile;
-	
-    private BufferedReader _reader;
-    
-    private ReadMode _readMode;
+
+	private BufferedReader _reader;
+
+	private ReadMode _readMode;
 
 	private boolean _run;
-    
-	public FileReaderThread(SourceModule sourceModule,File inputFile,ReadMode readMode)
+
+	private static int TIME_SPAN = 100;
+
+	/**
+	 * This constrcutor creates the FileReaderThread.
+	 * @param sourceModule Is the SourceModule to which the events shall be passed
+	 * @param inputFile Is the File from which too read the events
+	 * @param readMode Tells the FileReaderThread to run in either "BURST" or "REALTIME" mode
+	 */
+	public FileReaderThread(SourceModule sourceModule, File inputFile, ReadMode readMode)
 	{
-		this._logger = LogHelper.createLogger(this);
-	
+		this._logger = LogHelper.createLogger(this.getClass().getName());
+
 		this._readMode = readMode;
-		
+
 		this._sourceModule = sourceModule;
-		
+
 		this._inputFile = inputFile;
-		
+
 		this._run = true;
-		
-	}
-	
-	public void run()
-	{
 
-    	
-    	Date dateOfLastEvent = null;
-    	
-    	Date relativeDate = null;
-    	
-        try {
-
-        	this._reader = new BufferedReader(new FileReader(this._inputFile));
-        	
-            int lineNumber = 0;
-            
-            String line = null;
-
-            StringTokenizer eventTokenizer = null;
-            
-            while ((line = this._reader.readLine()) != null && this._run)
-            {
-                lineNumber++;
-                
-                eventTokenizer = new StringTokenizer(line,ValidEventPacket.EVENT_SEPARATOR);
-                
-                if(eventTokenizer.countTokens() != 3)
-                {
-                    this._logger.log(Level.WARNING,"Error while reading line " + lineNumber + ":");
-                    
-                    this._logger.log(Level.WARNING,"This line does not contain valid event data.");
-                    
-                    continue;
-                }
-                
-                String timeStampString = eventTokenizer.nextToken();
-                
-                if(timeStampString == null || timeStampString.equals(""))
-                {
-                    this._logger.log(Level.WARNING,"Error while reading timeStamp in line " + lineNumber + ":");
-                    
-                    this._logger.log(Level.WARNING,"The timeStamp is empty.");
-                    
-                    continue;
-                }
-                
-                String sensorDataTypeString = eventTokenizer.nextToken();
-                
-                if(sensorDataTypeString == null || sensorDataTypeString.equals(""))
-                {
-                    this._logger.log(Level.WARNING,"Error while reading SensorDataType in line " + lineNumber + ":");
-                    
-                    this._logger.log(Level.WARNING,"The SensorDataType is empty.");
-                    
-                    continue;
-                }
-                
-                String argListString = eventTokenizer.nextToken();
-                
-                if(argListString == null || argListString.equals(""))
-                {
-                    this._logger.log(Level.WARNING,"Error while reading argList in line " + lineNumber + ":");
-                    
-                    this._logger.log(Level.WARNING,"The argList is empty.");
-                    
-                    continue;
-                }
-                
-                Date timeStamp = null;
-                
-                try {
-                    timeStamp = new SimpleDateFormat(ValidEventPacket.DATE_FORMAT_PATTERN).parse(timeStampString);
-                }
-                catch (ParseException e) {
-                    
-                    this._logger.log(Level.WARNING,"Error while reading timeStamp in line " + lineNumber + ":");
-                    
-                    this._logger.log(Level.WARNING,"The timeStamp is invalid.");
-                    
-                    this._logger.log(Level.WARNING,e.getMessage());
-                    
-                    continue;
-                }
-                
-                StringTokenizer argListTokenizer = new StringTokenizer(argListString,ValidEventPacket.ARGLIST_SEPARATOR);
-                
-                String[] argListStringArray = new String[argListTokenizer.countTokens()];
-                
-                int i = 0;
-                
-                while(argListTokenizer.hasMoreTokens())
-                {
-                    argListStringArray[i++] = argListTokenizer.nextToken();
-                }
-                
-                List argList = Arrays.asList(argListStringArray);
-                
-                ValidEventPacket eventPacket = null;
-                
-                try {
-                    eventPacket = new ValidEventPacket(0,timeStamp,sensorDataTypeString,argList);
-                }
-                catch (IllegalEventParameterException e) {
-                    
-                    this._logger.log(Level.WARNING,"Error while generating eventz from line " + lineNumber + ":");
-                    
-                    this._logger.log(Level.WARNING,e.getMessage());
-                    
-                    continue;
-                }
-                
-                if(this._readMode != ReadMode.BURST)
-                {                	
-	                if(dateOfLastEvent != null)
-	                {
-	                	long eventDateDiff = eventPacket.getTimeStamp().getTime() - dateOfLastEvent.getTime();
-	                	
-	                	Date currentDate = new Date();
-	                	
-	                	long realDateDiff = currentDate.getTime() - relativeDate.getTime();
-	                	
-	                	while(realDateDiff < eventDateDiff && this._readMode != ReadMode.BURST && this._run)
-	                	{
-	                		try
-							{
-								Thread.sleep(100);
-								
-								currentDate = new Date();
-								
-								realDateDiff = currentDate.getTime() - relativeDate.getTime();
-							}
-							catch (InterruptedException e)
-							{
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-							}
-	                	}
-	                	
-	                }
-	               
-                }
-                
-                dateOfLastEvent = eventPacket.getTimeStamp();
-                
-                relativeDate = new Date();
-                
-                this._sourceModule.append(eventPacket);
-                
-                 
-                
-            }
-            this._sourceModule.deactivate();
-        }
-        catch (IOException e) {
-            this._logger.log(Level.WARNING,"Error while reading the file.");
-            
-            this._logger.log(Level.WARNING,e.getMessage());
-        }
 	}
 
 	/**
-	 * @param readMode
-	 * @param speed
+	 * @see java.lang.Thread#run()
+	 */
+	@Override
+	public void run()
+	{
+
+		// Stores the timestamp of the last read event
+		Date dateOfLastEvent = null;
+
+		// Stores the Date when the last event has been read in
+		Date relativeDate = null;
+
+		try
+		{
+
+			// create the Reader
+			this._reader = new BufferedReader(new FileReader(this._inputFile));
+
+			// count the linenumbers for eventual error messages
+			int lineNumber = 0;
+
+			String line = null;
+
+			/*
+			 *  The first level Tokenizer used to disassemble the line into
+			 *  the timestamp, the SensorDataType and the argList String.
+			 */
+			StringTokenizer eventTokenizer = null;
+
+			//  As long as there are more lines...
+			while ((line = this._reader.readLine()) != null && this._run)
+			{
+
+				lineNumber++;
+
+				// Get a new Tokenizer.
+				eventTokenizer = new StringTokenizer(line,
+						ValidEventPacket.EVENT_SEPARATOR);
+
+				// Check if the line is well formed.
+				if (eventTokenizer.countTokens() != 3)
+				{
+					this._logger.log(Level.WARNING, "Error while reading line " + lineNumber + ":");
+
+					this._logger.log(Level.WARNING, "This line does not contain valid event data.");
+
+					continue;
+				}
+
+				// Get the timestamp String.
+				String timeStampString = eventTokenizer.nextToken();
+
+				// Check if the timestamp String is well formed.
+				if (timeStampString == null || timeStampString.equals(""))
+				{
+					this._logger.log(Level.WARNING, "Error while reading timeStamp in line " + lineNumber + ":");
+
+					this._logger.log(Level.WARNING, "The timeStamp is empty.");
+
+					continue;
+				}
+
+				// Get the SensorDataType String.
+				String sensorDataTypeString = eventTokenizer.nextToken();
+
+				// Check if the SensorDataType String is well formed.
+				if (sensorDataTypeString == null || sensorDataTypeString.equals(""))
+				{
+					this._logger.log(Level.WARNING, "Error while reading SensorDataType in line " + lineNumber + ":");
+
+					this._logger.log(Level.WARNING, "The SensorDataType is empty.");
+
+					continue;
+				}
+
+				// Get the argList String.
+				String argListString = eventTokenizer.nextToken();
+
+				// Check if the argList String is well formed.
+				if (argListString == null || argListString.equals(""))
+				{
+					this._logger.log(Level.WARNING, "Error while reading argList in line " + lineNumber + ":");
+
+					this._logger.log(Level.WARNING, "The argList is empty.");
+
+					continue;
+				}
+
+				// Try to parse the timestamp String into a Date object.
+				Date timeStamp = null;
+
+				try
+				{
+					timeStamp = new SimpleDateFormat(
+							ValidEventPacket.DATE_FORMAT_PATTERN).parse(timeStampString);
+				}
+				catch (ParseException e)
+				{
+
+					this._logger.log(Level.WARNING, "Error while reading timeStamp in line " + lineNumber + ":");
+
+					this._logger.log(Level.WARNING, "The timeStamp is invalid.");
+
+					this._logger.log(Level.WARNING, e.getMessage());
+
+					continue;
+				}
+
+				// This second level Tokenizer is used to dissasemble the argList.
+				StringTokenizer argListTokenizer = new StringTokenizer(
+						argListString, ValidEventPacket.ARGLIST_SEPARATOR);
+
+				// The Array is used to temprarilly store the argList entries.
+				String[] argListStringArray = new String[argListTokenizer.countTokens()];
+
+				int i = 0;
+
+				while (argListTokenizer.hasMoreTokens())
+				{
+					argListStringArray[i++] = argListTokenizer.nextToken();
+				}
+
+				// Create a List object from the Array now containing the argList String entries.
+				List argList = Arrays.asList(argListStringArray);
+
+				// Try to create a ValidEventPacket object from the line's data.
+				ValidEventPacket eventPacket = null;
+
+				try
+				{
+					eventPacket = new ValidEventPacket(0, timeStamp,
+							sensorDataTypeString, argList);
+				}
+				catch (IllegalEventParameterException e)
+				{
+
+					this._logger.log(Level.WARNING, "Error while generating eventz from line " + lineNumber + ":");
+
+					this._logger.log(Level.WARNING, e.getMessage());
+
+					continue;
+				}
+
+				/*
+				 * When this module is in "BURST" mode we can continue to parse the next line.
+				 * If not we need to analyse the timestamp of the next line.
+				 */
+				if (this._readMode != ReadMode.BURST)
+				{
+					// If this was the first line, then no previous timestamp has been stored.
+					if (dateOfLastEvent != null)
+					{
+						// Get the time delta of the last event and the current event.
+						long eventDelta = eventPacket.getTimeStamp().getTime() - dateOfLastEvent.getTime();
+
+						// Get the current Date
+						Date currentDate = new Date();
+
+						// Get the delta of the time when the last event was parsed and now
+						long realDelta = currentDate.getTime() - relativeDate.getTime();
+
+						// Get the delta in real realtime and compare it to the event time delta.
+						while (realDelta < eventDelta && this._readMode != ReadMode.BURST && this._run)
+						{
+							try
+							{
+								// Wait some time and retry until the time has ellapsed.
+								Thread.sleep(TIME_SPAN);
+
+								currentDate = new Date();
+
+								realDelta = currentDate.getTime() - relativeDate.getTime();
+							}
+							catch (InterruptedException e)
+							{
+								// No problem if and interruption occures...
+							}
+						}
+
+					}
+
+				}
+
+				// Store the timestamp of the current event for the next loop.
+				dateOfLastEvent = eventPacket.getTimeStamp();
+
+				// Store the timestamp for the next loop.
+				relativeDate = new Date();
+
+				// Now pass the event to the SourceModule and into the ECG Lab.
+				this._sourceModule.append(eventPacket);
+
+			}
+
+			this._reader.close();
+
+			this._sourceModule.deactivate();
+		}
+		catch (IOException e)
+		{
+			this._logger.log(Level.SEVERE, "Error while reading the file: " + this._inputFile.getAbsolutePath());
+
+			this._logger.log(Level.FINEST, e.getMessage());
+		}
+		finally
+		{
+			if (this._reader != null)
+			{
+				try
+				{
+					this._reader.close();
+				}
+				catch (IOException e)
+				{
+					this._logger.log(Level.SEVERE, "Error while closing the file: " + this._inputFile.getAbsolutePath());
+
+					this._logger.log(Level.FINEST, e.getMessage());
+				}
+			}
+		}
+	}
+
+	/**
+	 * This method sets the ReadMode for the FileReaderThread.
+	 * @param readMode Is the ReadMode. Either "BURST" or "REALTIME".
 	 */
 	public void setMode(ReadMode readMode)
 	{
@@ -217,12 +293,12 @@ public class FileReaderThread extends Thread
 	}
 
 	/**
-	 * 
+	 * This stops the FileReaderThread.
 	 */
 	public void stopReader()
 	{
 		this._run = false;
-		
+
 	}
-	 
+
 }
--- ECG_FileSystemSourceModule/src/org/electrocodeogram/module/source/FileSystemSourceModule.java
+++ ECG_FileSystemSourceModule/src/org/electrocodeogram/module/source/FileSystemSourceModule.java
@@ -5,137 +5,153 @@ import java.io.File;
 import org.electrocodeogram.module.ModulePropertyException;
 
 /**
- *
+ * This class is an ECG module used to read back events from a file.
+ * 
  */
 public class FileSystemSourceModule extends SourceModule
 {
-	
+	/**
+	 * The module can operate in two different modes.
+	 * It can either read in events as fast as possible in "BURST" mode,
+	 * or it can read in the stored events in the same time intervalls
+	 * in which they where stored.
+	 * 
+	 * This "REALTIME" mode is very neccessary for playing back the
+	 * recorded events along with a video record for example. In that
+	 * case one can use the ManualAnnotatorSourceModule to manually
+	 * annotate the evtn stream with additional events.
+	 * 
+	 * The "BURST" mode is usefull for simply (re)-analysis of the
+	 * event stream.
+	 *
+	 */
 	public enum ReadMode
 	{
-		BURST, REALTIME
+		/**
+		 * In this mode the module will read in the stored events as fast as possible.
+		 */
+		BURST,
+		/**
+		 * In this mode the module will read in the events in the time intervalls in which
+		 * they were stored. 
+		 */
+		REALTIME
 	}
 
 	private FileReaderThread _readerThread;
-	
+
 	private File _inputFile;
 
 	private ReadMode _readMode;
 
 	/**
-     * @param arg0
-     * @param arg1
-     */
-    public FileSystemSourceModule(String arg0, String arg1)
-    {
-        super(arg0, arg1);
-        
-    }
+	 * The constructor creates the module instance.
+	 * It is not to be called by developers, instead it is called
+	 * from the ECG ModuleRegistry when the user requested
+	 * a new instance of this module.
+	 * @param id This is the unique String id of the module  
+	 * @param name This is the name which is given to the module instance
+	 */
+	public FileSystemSourceModule(String id, String name)
+	{
+		super(id, name);
 
-    /**
-     * @param propertyName 
-     * @param propertyValue 
-     * @throws ModulePropertyException 
-     * 
-     */
-    @Override
-    public void setProperty(String propertyName, String propertyValue) throws ModulePropertyException
-    {
-        if (propertyName.equals("Input File"))
-        {
-
-	        File propertyValueFile = new File(propertyValue);
-	
-	        this._inputFile = propertyValueFile;
-        
-        }
-        else if(propertyName.equals("Enable Realtime Mode"))
-        {
-        	if(propertyValue.equalsIgnoreCase("true"))
-        	{
-        		this._readMode = ReadMode.REALTIME;
-        		
-        		setMode();
-        	}
-        }
-        else if(propertyName.equals("Enable Burst Mode"))
-        {
-        	if(propertyValue.equalsIgnoreCase("true"))
-        	{
-        		this._readMode = ReadMode.BURST;
-        		
-        		setMode();
-        	}
-        }
-        else
-        {
-        	throw new ModulePropertyException(
-                    "The module does not support a property with the given name: " + propertyName);
-        }
-               
-    }
-
- 
+	}
 
 	/**
-	 * 
+	 * @see org.electrocodeogram.module.Module#setProperty(java.lang.String, java.lang.String)
 	 */
+	@Override
+	public void setProperty(String propertyName, String propertyValue) throws ModulePropertyException
+	{
+		if (propertyName.equals("Input File"))
+		{
+
+			File propertyValueFile = new File(propertyValue);
+
+			this._inputFile = propertyValueFile;
+
+		}
+		else if (propertyName.equals("Enable Realtime Mode"))
+		{
+			if (propertyValue.equalsIgnoreCase("true"))
+			{
+				this._readMode = ReadMode.REALTIME;
+
+				setMode();
+			}
+		}
+		else if (propertyName.equals("Enable Burst Mode"))
+		{
+			if (propertyValue.equalsIgnoreCase("true"))
+			{
+				this._readMode = ReadMode.BURST;
+
+				setMode();
+			}
+		}
+		else
+		{
+			throw new ModulePropertyException(
+					"The module does not support a property with the given name: " + propertyName);
+		}
+
+	}
+
 	private void setMode()
 	{
-		if(this._readerThread != null)
+		if (this._readerThread != null)
 		{
 			this._readerThread.setMode(this._readMode);
 		}
-		
+
 	}
 
-	public void analyseCoreNotification()
-    {
-
-    }
-
-    /**
-     * @see org.electrocodeogram.module.Module#initialize()
-     */
-    @Override
-    public void initialize()
-    {
-        
-    }
-
-    /**
-     * @throws SourceModuleException 
-     * @throws SourceModuleException 
-     * @see org.electrocodeogram.module.source.SourceModule#startReader(org.electrocodeogram.module.source.SourceModule)
-     */
-    @Override
-    public void startReader(SourceModule arg0) throws SourceModuleException
-    {
-		if(this._inputFile == null)
-    	{
-    		throw new SourceModuleException("No input file selected");
-    	}
-
-		this._readerThread = new FileReaderThread(this,this._inputFile,this._readMode);
-		
-		this._readerThread.start();
-    }
-    
-    /**
-	 * @see org.electrocodeogram.module.Module#getProperty(java.lang.String)
+	/**
+	 *  @see org.electrocodeogram.module.Module#analyseCoreNotification()
+	 *  This method is not implemented in this module.
 	 */
 	@Override
-	public String getProperty(String propertyName)
+	public void analyseCoreNotification()
 	{
-		return null;
+		// not implemented
 	}
 
-	/* (non-Javadoc)
+	/**
+	 * @see org.electrocodeogram.module.Module#initialize()
+	 *  This method is not implemented in this module.
+	 */
+	@Override
+	public void initialize()
+	{
+		// not implemented
+	}
+
+	/**
+	 * @see org.electrocodeogram.module.source.SourceModule#startReader(org.electrocodeogram.module.source.SourceModule)
+	 */
+	@Override
+	public void startReader(SourceModule sourceModule) throws SourceModuleException
+	{
+		if (this._inputFile == null)
+		{
+			throw new SourceModuleException("No input file selected");
+		}
+
+		this._readerThread = new FileReaderThread(sourceModule,
+				this._inputFile, this._readMode);
+
+		this._readerThread.start();
+	}
+
+	
+	/**
 	 * @see org.electrocodeogram.module.source.SourceModule#stopReader()
 	 */
 	@Override
 	public void stopReader()
 	{
 		this._readerThread.stopReader();
-		
+
 	}
 }
