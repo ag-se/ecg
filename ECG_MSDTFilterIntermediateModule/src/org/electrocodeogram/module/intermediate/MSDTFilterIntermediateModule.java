064d8f804ae5ae1f294464b1b1308ef0eab8af1e
its7oas7er <its7oas7er@3537aeea-94f2-0310-a3c2-ed66167d6be7>
2005-10-20 20:26:13 +0000

    git-svn-id: svn://svn.berlios.de/ecg/trunk@515 3537aeea-94f2-0310-a3c2-ed66167d6be7

--- ECG_MSDTFilterIntermediateModule/src/org/electrocodeogram/module/intermediate/MSDTFilterIntermediateModule.java
+++ ECG_MSDTFilterIntermediateModule/src/org/electrocodeogram/module/intermediate/MSDTFilterIntermediateModule.java
@@ -16,8 +16,9 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import org.electrocodeogram.event.ValidEventPacket;
+import org.electrocodeogram.module.ModuleProperty;
 import org.electrocodeogram.msdt.MicroSensorDataType;
-import org.electrocodeogram.system.SystemRoot;
+import org.electrocodeogram.system.ModuleSystem;
 
 
 
@@ -69,9 +70,9 @@ public class MSDTFilterIntermediateModule extends IntermediateModule
      * @param propertyValue
      */
     @Override
-    public void setProperty(String propertyName, String propertyValue)
+    public void propertyChanged(ModuleProperty moduleProperty)
     {
-       if(propertyValue.equals("configureFilter"))
+       if(moduleProperty.getValue().equals("configureFilter"))
        {
            configureFilter();
        }
@@ -99,7 +100,7 @@ public class MSDTFilterIntermediateModule extends IntermediateModule
     {
         this.msdtFilterMap = new HashMap<MicroSensorDataType,Boolean>();
         
-        MicroSensorDataType[] msdts = SystemRoot.getModuleInstance().getModuleMsdtRegistry().getMicroSensorDataTypes();
+        MicroSensorDataType[] msdts = ModuleSystem.getInstance().getMicroSensorDataTypes();
         
         for(MicroSensorDataType msdt : msdts)
         {
@@ -130,14 +131,14 @@ public class MSDTFilterIntermediateModule extends IntermediateModule
     {
         if(this.msdtFilterMap.size() == 0)
         {
-            JOptionPane.showMessageDialog(SystemRoot.getModuleInstance().getRootFrame(),"There are no MicroSensorDataTypes loaded yet. Please add at least one SourceModule to load the core MicroSensorDataTypes.","Configure Filter Message",JOptionPane.INFORMATION_MESSAGE);
+            JOptionPane.showMessageDialog(ModuleSystem.getInstance().getRootFrame(),"There are no MicroSensorDataTypes loaded yet. Please add at least one SourceModule to load the core MicroSensorDataTypes.","Configure Filter Message",JOptionPane.INFORMATION_MESSAGE);
             
             return;
         }
         
         initializeCheckBoxes();
         
-        this.dlgFilterConfiguration = new JDialog(SystemRoot.getModuleInstance().getRootFrame(),"Configure Filter");
+        this.dlgFilterConfiguration = new JDialog(ModuleSystem.getInstance().getRootFrame(),"Configure Filter");
         
         this.pnlCheckBoxes = createMainPanel();
         
