da8079d522074cb5a839347a43165f57675faf11
jekutsch <jekutsch@3537aeea-94f2-0310-a3c2-ed66167d6be7>
2006-03-01 17:27:38 +0000

    v1.0.1 upload
    git-svn-id: svn://svn.berlios.de/ecg/trunk@748 3537aeea-94f2-0310-a3c2-ed66167d6be7

---
+++ modules/ECG_ManualAnnotatorSourceModule/src/org/electrocodeogram/module/source/implementation/ManualAnnotatorSourceModule.java
@@ -0,0 +1,158 @@
+/*
+ * Class: ManualAnnotatorSourceModule
+ * Version: 1.0
+ * Date: 16.10.2005
+ * By: Frank@Schlesinger.com
+ */
+
+package org.electrocodeogram.module.source.implementation;
+
+import java.util.logging.Logger;
+
+import org.electrocodeogram.logging.LogHelper;
+import org.electrocodeogram.modulepackage.ModuleProperty;
+import org.electrocodeogram.modulepackage.ModulePropertyException;
+import org.electrocodeogram.module.source.EventReader;
+import org.electrocodeogram.module.source.SourceModule;
+
+/**
+ * This module is used to manually add events into the event stream.
+ * For sure there are many kind of relevant events that are hard to
+ * be recorded programmtically from a sensor.<br>
+ * For example: It might be very interessting to notice, when
+ * the programmer is interrupted from his work, but this of course
+ * is hard to figure aut from inside a programm.
+ * <br>
+ * Those events can be defined in this module's <em>ModuleDescription</em>.
+ * For every event that is specified there, the user will get a button
+ * in a dialog window in the ECG Lab's GUI.
+ * <br>
+ * So at every time the user observes an interesting event he or she
+ * simply pushes the button and the module is sending out the event.  
+ */
+public class ManualAnnotatorSourceModule extends SourceModule {
+
+    /**
+     * A reference to the <em>EventReader</em>.
+     */
+    private ManualReader eventReader;
+
+    /**
+     * This is the logger.
+     */
+    private static Logger logger = LogHelper
+        .createLogger(ManualAnnotatorSourceModule.class.getName());
+
+    /**
+     * The creates the module instance. It is not to be
+     * called by developers, instead it is called from the {@link ModuleRegistry}
+     * when the user requested a new instance of this
+     * module.
+     * @param id
+     *            This is the unique <code>String</code> id of the module
+     * @param name
+     *            This is the name which is assigned to the module
+     *            instance
+     */
+    public ManualAnnotatorSourceModule(String id, String name) {
+        super(id, name);
+
+        logger.entering(this.getClass().getName(),
+            "ManualAnnotatorSourceModule", new Object[] {id, name});
+
+        logger
+            .exiting(this.getClass().getName(), "ManualAnnotatorSourceModule");
+
+    }
+
+    /**
+     * @see org.electrocodeogram.module.Module#propertyChanged(org.electrocodeogram.module.ModuleProperty)
+     */
+    @Override
+    public void propertyChanged(ModuleProperty moduleProperty)
+        throws ModulePropertyException {
+
+        logger.entering(this.getClass().getName(), "propertyChanged",
+            new Object[] {moduleProperty});
+
+        if (moduleProperty.getName().equals("Show Dialog")) {
+            if (!this.isActive()) {
+
+                logger.exiting(this.getClass().getName(), "propertyChanged");
+
+                throw new ModulePropertyException(
+                    "The module has not been started yet.", this.getName(),
+                    this.getId(), moduleProperty.getName(), moduleProperty
+                        .getValue());
+            }
+
+            this.eventReader.showDialog();
+        }
+
+        logger.exiting(this.getClass().getName(), "propertyChanged");
+    }
+
+    /**
+     * @see org.electrocodeogram.module.Module#update()
+     *      This method is not implemented in this module, as
+     *      this module does not need to be informed about
+     *      ECG Lab subsystem's state changes.
+     */
+    @Override
+    public void update() {
+        // not implemented
+    }
+
+    /**
+     * @see org.electrocodeogram.module.Module#initialize() This
+     *      method is not implemented in this module.
+     */
+    @Override
+    public void initialize() {
+    // not implemented
+    }
+
+    /**
+     * @see org.electrocodeogram.module.source.SourceModule#getEventReader()
+     */
+    @Override
+    public EventReader[] getEventReader() {
+
+        logger.entering(this.getClass().getName(), "getEventReader");
+
+        if (this.eventReader == null) {
+            this.eventReader = new ManualReader(this);
+        }
+
+        logger.exiting(this.getClass().getName(), "getEventReader",
+            new EventReader[] {this.eventReader});
+
+        return new EventReader[] {this.eventReader};
+    }
+
+    /**
+     * @see org.electrocodeogram.module.source.SourceModule#preStart()
+     * This
+     *      method is not implemented in this module.
+     */
+    @Override
+    public void preStart() {
+    // not implemented
+    }
+
+    /**
+     * @see org.electrocodeogram.module.source.SourceModule#postStop()
+     */
+    @Override
+    public void postStop() {
+
+        logger.entering(this.getClass().getName(), "postStop");
+
+        this.eventReader.hideDialog();
+
+        this.eventReader = null;
+
+        logger.exiting(this.getClass().getName(), "postStop");
+
+    }
+}
