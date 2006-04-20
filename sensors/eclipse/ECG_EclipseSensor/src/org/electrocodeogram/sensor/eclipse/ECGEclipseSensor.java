97f665bd5735e174262e4a6ddc48dba76de4cd3b
jekutsch <jekutsch@3537aeea-94f2-0310-a3c2-ed66167d6be7>
2006-04-20 12:55:15 +0000

    Now creates 'antrun' and the new 'run' events. Features: * Each run has an unique id * The termination of a run/antrun is reported in a second event with the same id * Includes run targets/names * profile runs are reported just like run and debug runs have been before
    git-svn-id: svn://svn.berlios.de/ecg/trunk@813 3537aeea-94f2-0310-a3c2-ed66167d6be7

--- sensors/eclipse/ECG_EclipseSensor/src/org/electrocodeogram/sensor/eclipse/ECGEclipseSensor.java
+++ sensors/eclipse/ECG_EclipseSensor/src/org/electrocodeogram/sensor/eclipse/ECGEclipseSensor.java
@@ -13,6 +13,8 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
@@ -26,6 +28,7 @@ import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.IDebugEventSetListener;
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.debug.core.ILaunchManager;
+import org.eclipse.debug.core.model.IProcess;
 import org.eclipse.debug.core.model.RuntimeProcess;
 import org.eclipse.jdt.internal.junit.ui.JUnitPlugin;
 import org.eclipse.jdt.junit.ITestRunListener;
@@ -857,6 +860,7 @@ public final class ECGEclipseSensor {
 
         /**
          * @see org.eclipse.debug.core.IDebugEventSetListener#handleDebugEvents(org.eclipse.debug.core.DebugEvent[])
+         * 
          */
         @SuppressWarnings("synthetic-access")
         public void handleDebugEvents(final DebugEvent[] events) {
@@ -871,19 +875,29 @@ public final class ECGEclipseSensor {
 
                 return;
             }
+                        
+            // TODO: Allow for more than one parallel launch
+            for (int i=0; i < events.length; i++) {
+                Object source = events[i].getSource();
 
-            // TODO: What about the others?
-            Object source = events[0].getSource();
+                if (source instanceof RuntimeProcess) {
+                    RuntimeProcess rp = (RuntimeProcess) source;
 
-            if (source instanceof RuntimeProcess) {
-                RuntimeProcess rp = (RuntimeProcess) source;
-
-                ILaunch launch = rp.getLaunch();
-
-                if (this.currentLaunch == null || !this.currentLaunch.equals(launch)) {
-                    this.currentLaunch = launch;
-                    analyseLaunch(launch);
-                }
+                    ILaunch launch = rp.getLaunch();
+                    
+                    if (launch.equals(this.currentLaunch) && events[i].getKind() == DebugEvent.TERMINATE) {
+                    	// An active launch got a TERMINATE event
+                    	if (analyseTermination(rp, launch))
+                    		this.currentLaunch = null;
+                    } 
+                    else if (this.currentLaunch == null || !this.currentLaunch.equals(launch)) {
+                    	// There are many events on the same process and launch.
+                    	// analyseLaunch returns true if this launch has been recognized
+                    	// Otherwise re-analyse every following similiar event
+                    	if (analyseLaunch(rp, launch))
+                    		this.currentLaunch = launch;
+                    }
+                }            	
             }
 
             logger.exiting(this.getClass().getName(), "handleDebugEvents");
@@ -894,9 +908,11 @@ public final class ECGEclipseSensor {
          * determines if it is a run or a debug launch.
          * @param launch
          *            Is the launch to analyse
+         * @param process the IProcess for this launch
+         * @returns true if event has been recognized and processed
          */
         @SuppressWarnings("synthetic-access")
-        private void analyseLaunch(final ILaunch launch) {
+        private boolean analyseLaunch(final RuntimeProcess process, final ILaunch launch) {
             logger.entering(this.getClass().getName(), "analyseLaunch");
 
             if (launch == null) {
@@ -904,36 +920,140 @@ public final class ECGEclipseSensor {
                     "The Parameter lauch is null. Ignoring event.");
 
                 logger.exiting(this.getClass().getName(), "analyseLaunch");
-
-                return;
+                return false;
             }
 
-            if (launch.getLaunchMode().equals(ILaunchManager.RUN_MODE)) {
+            if (process.getAttribute(IProcess.ATTR_PROCESS_TYPE).toLowerCase().endsWith("antprocess")) {
+
+                String cmdLine = process.getAttribute(IProcess.ATTR_CMDLINE);
+            	
+            	if (cmdLine == null) {
+            		// It may occur that the process attribute fields are not set
+            		// return false to wait for the next event of a DebugEvent.CHANGE kind
+            		// which may be a change of the attribute settings
+            		return false;
+            	}
+
+                logger.log(ECGLevel.PACKET, "An ant event has been recorded.");
+
+            	// extract Ant's buildfile and target settings from the 
+            	// java-call of Ant
+            	String buildfile = "";
+            	String target = "";
+            	String[] args = DebugPlugin.parseArguments(cmdLine);
+                for (int i=0; i < args.length; i++) {
+                	if (args[i].equals("-buildfile") 
+                			&& i+1 < args.length 
+                			&& args[i+1] != null) {
+                		buildfile = args[i+1]; 
+                	}
+                	if (i+1 == args.length && !args[i-1].startsWith("-")) {
+                		target = args[i];
+                	}
+                }
+                
+                processActivity(
+                    "msdt.antrun.xsd",
+                    "<?xml version=\"1.0\"?><microActivity><commonData><username>"
+                        + ECGEclipseSensor.this.username
+                        + "</username><projectname>"
+                        + ECGEclipseSensor.this.projectname
+                        + "</projectname></commonData><ant>"
+                        + "<id>" + launch.hashCode() + "</id>"
+                        + "<mode>" + launch.getLaunchMode() + "</mode>"
+                        + "<buildfile>" + buildfile + "</buildfile>"
+                        + "<target>" + target + "</target>"
+                        + "</ant></microActivity>");
+            	
+            }
+            else {
 
                 logger.log(ECGLevel.PACKET, "A run event has been recorded.");
 
+                String config = "null";
+                if (launch.getLaunchConfiguration() != null)
+                	config = launch.getLaunchConfiguration().getName();
+                
                 processActivity(
-                    "msdt.rundebug.xsd",
+                    "msdt.run.xsd",
                     "<?xml version=\"1.0\"?><microActivity><commonData><username>"
-                                    + ECGEclipseSensor.this.username
-                                    + "</username><projectname>"
-                                    + ECGEclipseSensor.this.projectname
-                                    + "</projectname></commonData><run debug=\"false\"></run></microActivity>");
-            } else if (launch.getLaunchMode().equals(ILaunchManager.DEBUG_MODE)) {
-                logger.log(ECGLevel.PACKET, "A debug event has been recorded.");
-
-                processActivity(
-                    "msdt.rundebug.xsd",
-                    "<?xml version=\"1.0\"?><microActivity><commonData><username>"
-                                    + ECGEclipseSensor.this.username
-                                    + "</username><projectname>"
-                                    + ECGEclipseSensor.this.projectname
-                                    + "</projectname></commonData><run debug=\"true\"></run></microActivity>");
+                        + ECGEclipseSensor.this.username
+                        + "</username><projectname>"
+                        + ECGEclipseSensor.this.projectname
+                        + "</projectname></commonData><run>"
+                        + "<id>" + launch.hashCode() + "</id>"
+                        + "<mode>" + launch.getLaunchMode() + "</mode>"
+                        + "<launch>" + config + "</launch>"
+                        + "</run></microActivity>");
             }
-            // There's "profile" mode as well which is being ignored.
 
             logger.exiting(this.getClass().getName(), "analyseLaunch");
+            
+            return true;
         }
+
+        /**
+         * This method is analysing a program termination.
+         * @param launch Is the launch to analyse
+         * @param process the IProcess for this launch
+         * @returns true if event has been recognized and processed
+         */
+        @SuppressWarnings("synthetic-access")
+        private boolean analyseTermination(final RuntimeProcess process, final ILaunch launch) {
+            logger.entering(this.getClass().getName(), "analyseTermination");
+
+            if (launch == null) {
+                logger.log(Level.FINE,
+                    "The Parameter lauch is null. Ignoring event.");
+
+                logger.exiting(this.getClass().getName(), "analyseTermination");
+                return false;
+            }
+
+            if (process.getAttribute(IProcess.ATTR_PROCESS_TYPE).toLowerCase().endsWith("antprocess")) {
+
+                logger.log(ECGLevel.PACKET, "An ant termination event has been recorded.");
+
+                processActivity(
+                    "msdt.antrun.xsd",
+                    "<?xml version=\"1.0\"?><microActivity><commonData><username>"
+                        + ECGEclipseSensor.this.username
+                        + "</username><projectname>"
+                        + ECGEclipseSensor.this.projectname
+                        + "</projectname></commonData><ant>"
+                        + "<id>" + launch.hashCode() + "</id>"
+                        + "<mode>termination</mode>"
+                        + "<buildfile></buildfile>"
+                        + "<target></target>"
+                        + "</ant></microActivity>");
+            	
+            }
+            else {
+
+                logger.log(ECGLevel.PACKET, "A run/debug termination event has been recorded.");
+
+                String config = "null";
+                if (launch.getLaunchConfiguration() != null)
+                	config = launch.getLaunchConfiguration().getName();
+                
+                processActivity(
+                    "msdt.run.xsd",
+                    "<?xml version=\"1.0\"?><microActivity><commonData><username>"
+                        + ECGEclipseSensor.this.username
+                        + "</username><projectname>"
+                        + ECGEclipseSensor.this.projectname
+                        + "</projectname></commonData><run>"
+                        + "<id>" + launch.hashCode() + "</id>"
+                        + "<mode>termination</mode>"
+                        + "<launch>" + config + "</launch>"
+                        + "</run></microActivity>");
+            }
+            
+            logger.exiting(this.getClass().getName(), "analyseTermination");
+            
+            return true;
+        }
+
     }
 
     /**
