c39b7a21ab7080429d75ec7b39fbe3ef5e4417e7
jule <jule@3537aeea-94f2-0310-a3c2-ed66167d6be7>
2006-10-31 17:39:04 +0000

    git-svn-id: svn://svn.berlios.de/ecg/trunk@975 3537aeea-94f2-0310-a3c2-ed66167d6be7

---
+++ modules/ECG_DatabaseTargetModule/src/org/electrocodeogram/module/target/implementation/DBTargetModuleConstants.java
@@ -0,0 +1,35 @@
+package org.electrocodeogram.module.target.implementation;
+
+
+public class DBTargetModuleConstants {
+	
+	//------------Constants for the DB Communicator ----------------------------
+	/**
+     * the used JDBC Driver
+     */
+    public static final String DB_DRIVER = "com.mysql.jdbc.Driver";
+
+    /**
+     * Database URL
+     */
+    public static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/ecg_test";
+
+    /**
+     * username to connect the database
+     */
+    public static final String DB_USER = "ecg";
+
+    /**
+     * password to connect the database
+     */
+    public static final String DB_PWD = "ecg";
+
+    
+//  ----------------------------------------------------------------------------
+    
+// -------------------------Constants for the DBTargetModule -------------------
+    public static final String MSDT_FOLDER = "../ECG_Lab/msdt/";
+// -----------------------------------------------------------------------------
+	
+
+}
