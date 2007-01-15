705cc70fd2d8f1803604bd7ea4fbfc8dca48575e
papadopoulos <papadopoulos@3537aeea-94f2-0310-a3c2-ed66167d6be7>
2007-01-15 13:10:07 +0000

    The class Clone.java is a container for the representation of duplicated codesnippets in an original source code. These codesnippets are called clones. They are observed by CopyPasteChangeRecognizer objects.
    git-svn-id: svn://svn.berlios.de/ecg/trunk@1042 3537aeea-94f2-0310-a3c2-ed66167d6be7

--- modules/ECG_EpisodeRecognizerIntermediateModule/src/org/electrocodeogram/module/intermediate/implementation/recognizers/Clone.java
+++ modules/ECG_EpisodeRecognizerIntermediateModule/src/org/electrocodeogram/module/intermediate/implementation/recognizers/Clone.java
@@ -1,279 +1,317 @@
+/*
+ * (c) Freie Universität Berlin - AG SoftwareEngineering - 2007
+ *
+ *
+ *  Diplomarbeit: Verfolgen von Kodekopien in Eclipse
+ *  @author Sofoklis Papadopoulos
+ *
+ *
+ */
+
 package org.electrocodeogram.module.intermediate.implementation.recognizers;
 
 import java.util.*;
 
-/**
- * 
- * @author S. Papadopoulos
- * Allgemein:
- * 1. Codedifferenzen können einen Clone ungültig werden lassen (invalid).
- *    Das Berechnen von Codedifferenzen wird bisher durch die Leventhstein-Distanz durchgeführt.
- * 2. Das Ungültigwerden eines Events bezieht sich stets auf andere Kopien im Recognizer.
- * 3. Sobald eine Kopie im Recognizer ex., die dem betrachteten Eventcode über 50% (oder andere Schwelle) ähnelt,
- *    muss der betrachtete Clone gültig (valid) bleiben. So kann der Recognizer bei einer nächsten Codecänderung
- *    diesen Eventcode wieder einbeziehen. Es können durchaus noch mehrere ähnliche Codecs im Recognizer enthalten sein.
- * 4. D.h., ist ein Clone gültig, ex. mindestens eine weitere ähnliche und beobachtete Codekopie im Recognizer.
- * 5. D.h., ist ein Clone ungültig, ex. kein ähnlicher beobachteter Eventcode mehr im Recognizer.
- * 6. Bei jeder Codeänderung müssen alle Eventcodes (valid & invalid) verglichen werden, da sich während der Codeverfolgung Änderungen an EventCodes
- *    auftreten können, die ein zuvor ungültigen Clone wieder gültig werden lassen !
- *    D.h., alle EventCodes müssen in linediffs einbezogen werden, unabhängig davon, ob sie invalid oder valid sind (q.d.e.).
- * 7. D.h., nur ein vollständig gelöschter EventCode überführt einen Clone in einen irreversiblen ungültigen Zustand (absolute_invalid).
- * 8. Obiges Verhalten implementiert ein UNDO ohne eine UNDO-Aktion erhalten zu haben.
- * n. Das Löschen des kompletten Codes läßt den Clone immer ungültig werden (invalid).
- */
 
 public class Clone {
-	  
-	  private String eventCode;
-	  /**public enum EventType {
-		  CUT,
-		  COPY,
-		  PASTE,
-		  CHANGE
-	  }*/
-	 //private int statisticEventCount = 0;
-	 private int statisticSibblingCount = 0;
-	  
-	 private String eventFilename;
-	 private String eventUserName;
-	 private String eventProjectName;
-	 private String eventActivityName;
-	 private int eventCodeStartline;
-	 private int eventCodeEndline;
-	 private Date eventStartDate;
+	 /**
+     * The code of clone.
+     */ 
+     private String cloneCode;
+     /**
+      * This is the filename where the clone belongs to.
+      */
+	 private String cloneFilename;
+	 /**
+	  * This is the startline of the clones code.
+	  */
+	 private int cloneCodeStartline;
+	 /**
+	  * This is the endline of the clones code.
+	  */
+	 private int cloneCodeEndline;
+	 /**
+      * This is the creation date of the clone.
+      */
+	 private Date cloneStartDate;
+	 /**
+	  * This is a boolean that determines if a clone is under observation
+	  */
 	 private boolean valid;
+	 /**
+	  * This is a boolean that determines if a clone is deleted.
+	  */
 	 private boolean deleted;
-	 
-	 //private boolean valid = false;
-	 //private Vector vSibblings;
+	 /**
+      * This is a container for the code of the clones. For convenient code line evaluation.
+      */
 	 private Vector<String> vLines;
+	 /**
+	  * This is a Container for the deleted lines in a clone. 
+	  */
 	 private Vector<Boolean> vLinesDeleted;
-	   		 
-	 /*protected Clone (String activityCode, String user, String project, String activity, String file, int startL, int endL, Date date){
-		 this.eventCode = activityCode;
-		 this.eventUserName = user;
-		 this.eventProjectName = project; 
-		 this.eventActivityName = activity;
-		 this.eventFilename = file;
-		 this.eventCodeStartline = startL;
-		 this.eventCodeEndline = endL;
-		 this.eventStartDate = date;
-		 //this.vSibblings = null;
-		 this.deleted = false;
-		 this.valid = true;
-		 buildEventCodeLines();
-		 calcEventEndLineNumber();
-		 //statisticEventCount++;
-	 }*/
+	 /**
+	  * 
+	  * @param copied if copy state
+	  * @param cutted if cut state
+	  * @param cut_pasted if cut_pasted state
+	  * @param pasted if pasted state
+	  * @param file filename whre the clon ebelongs to
+	  * @param startL startline of the clone
+	  * @param endL endline of the clone
+	  * @param code text of the clone
+	  * @param creationDate timestamp of clone creation
+	  */
 	 
-	 public Clone (boolean copied, boolean cutted, boolean cut_pasted, boolean pasted, String file, int startL, int endL, String code, Date creationDate){
-		 this.eventCode = code;
-		 this.eventFilename = file;
-		 this.eventCodeStartline = startL;
-		 this.eventCodeEndline = endL;
+	 public Clone (boolean copied, boolean cutted, boolean cut_pasted, boolean pasted, String file, 
+			                                   int startL, int endL, String code, Date creationDate){
+		 this.cloneCode = code;
+		 this.cloneFilename = file;
+		 this.cloneCodeStartline = startL;
+		 this.cloneCodeEndline = endL;
 		 this.deleted = false;
 		 this.valid = true;
-		 this.eventStartDate = creationDate;
-		 buildEventCodeLines();
-		 calcEventEndLineNumber();
+		 this.cloneStartDate = creationDate;
+		 //fill 
+		 buildCloneCodeLines();
+		 calcCloneEndLineNumber();
 	 }
+	 
 	 
 	 public String toString(){
-		 return "datei: " + eventFilename + " startzeile: " + eventCodeStartline + " endzeile: " + eventCodeEndline + " code: " + eventCode;
+		 return "datei: " + cloneFilename + " startzeile: " + cloneCodeStartline + " endzeile: " + cloneCodeEndline + " code: " + cloneCode;
 	 }
 	 
+	 /**
+	  * @param isValid To set the boolean valid.
+	  */
 	 public void setValid(boolean isValid) {
 		 this.valid = isValid;
 	 }
-	 
+	 /**
+	  * @param isDeleted To set the boolean isDeleted.
+	  */
 	 public void setDeleted(boolean isDeleted){
 		 this.deleted = isDeleted;
 	 }
-	 
+	 /**
+	  * 
+	  * @return True,if clone is deleted otherwise false.
+	  */
 	 public boolean isDeleted() {return this.deleted;}
-	 
+	 /**
+	  * 
+	  * @return True,if clone is valid otherwise false.
+	  */
 	 public boolean isValid() {return this.valid;}
 	 
+	 /**
+	  * Decrement the line numbers of a clone.
+	  *
+	  */
 	 public void decrementLinenumbers() {
-		 this.eventCodeStartline--;
-		 this.eventCodeEndline--;
+		 this.cloneCodeStartline--;
+		 this.cloneCodeEndline--;
 	 }
-	 
+	 /**
+	  * 
+	  * Increment the line numbers of a clone.
+	  */
 	 public void incrementLinenumbers() {
-		 this.eventCodeStartline++;
-		 this.eventCodeEndline++;
+		 this.cloneCodeStartline++;
+		 this.cloneCodeEndline++;
+	 }
+
+	 public void setCloneStartDate(Date startDate) {
+		 this.cloneStartDate = startDate;
+	 }
+
+	 public Date getCloneStartDate() {
+		 return this.cloneStartDate;
+	 }
+
+	 public int getCloneCodeStartline() {
+		 return this.cloneCodeStartline;
 	 }
 	 
-	 public void setEventStartDate(Date startDate) {
-		 this.eventStartDate = startDate;
+	 public int getCloneCodeEndline() {
+		 return this.cloneCodeEndline;
 	 }
 	 
-	 public Date getEventStartDate() {
-		 return this.eventStartDate;
+	 public String getCloneFilename() {
+		 return this.cloneFilename;
 	 }
-	 
-	 public int getEventCodeStartline() {
-		 return this.eventCodeStartline;
-	 }
-	 
-	 public int getEventCodeEndline() {
-		 return this.eventCodeEndline;
-	 }
-	 
-	 public String getEventFilename() {
-		 return this.eventFilename;
-	 }
-	 
-	 private void buildEventCodeLines(){
-		if(this.eventCode != null){ 
-			//StringTokenizer eventCodeLines = new StringTokenizer(eventCode, "\\n", false);
-			StringTokenizer eventCodeLines = new StringTokenizer(eventCode, "\n", false);
+	 /**
+	  * 
+	  * Because of linewise evaluation the codelines of the clones are stored in a vector.
+	  */
+	 private void buildCloneCodeLines(){
+		if(this.cloneCode != null){ 
+			StringTokenizer cloneCodeLines = new StringTokenizer(cloneCode, "\n", false);
 			vLines = new Vector<String>();
 			vLinesDeleted = new Vector<Boolean>();
-          while(eventCodeLines.hasMoreTokens()){
-          	vLines.add(eventCodeLines.nextToken());
+          while(cloneCodeLines.hasMoreTokens()){
+          	vLines.add(cloneCodeLines.nextToken());
           	vLinesDeleted.add(new Boolean(false));
           }
 		}
 	 }
-	 
-	 private void calcEventEndLineNumber() {
+	 /**
+	  * 
+	  * When creating a clone calculate the end line because in a textoperation, there exist no endline of the copied, pasted, cutted clone
+	  */  
+	 private void calcCloneEndLineNumber() {
 		 if (vLines != null)
-			 eventCodeEndline = eventCodeStartline + vLines.size() - 1;
+			 cloneCodeEndline = cloneCodeStartline + vLines.size() - 1;
 	 }
 	 
-	 public int getEventEndLine(){
+	 public int getCloneEndLine(){
 		 if(vLines != null){
 			 int vLinesSize = 0;
-			 return vLinesSize = eventCodeStartline + vLines.size() -1;
+			 return vLinesSize = cloneCodeStartline + vLines.size() -1;
 		 }
 		 else return 0;
 	 }
 	 
+	 
 	 public boolean deleteLine(int linenumber) {
+     //A linediff delete is recognized. If linenumber < startline the linediff-event takes place outside this clone.
 		 boolean inside = false;
-		 if (linenumber < this.eventCodeStartline) {
+		 if (linenumber < this.cloneCodeStartline) {
 			 this.decrementLinenumbers();
-			 System.out.println("event -> zeile: " + linenumber + " vor beobachtetem textblock gelöscht & zeilen dekrementiert");
 		 }
-		 else if (linenumber >= this.eventCodeStartline && linenumber <= this.eventCodeEndline)
-			 //inside = deleteEventCodeLine(linenumber);
-			 inside = markEventCodeLineAsDeleted(linenumber);
+		 else if (linenumber >= this.cloneCodeStartline && linenumber <= this.cloneCodeEndline)
+			 inside = markCloneCodeLineAsDeleted(linenumber);
 		 return inside;
 	 }
-	 
+	 /**
+	  * @param lineNumber and codeline
+	  * The index in vLines and the new code line to insert.
+	  * @return boolean True, if insertion inside of a clone, otherwise false.  
+	  */
 	 public boolean insertLine(int linenumber, String codeLine) {
+     //A linediff insert is recognized if linenumber < startline linediff-event takes place outside the clone.
 		 boolean inside = false;
-		 if (linenumber < this.eventCodeStartline) {
+		 if (linenumber < this.cloneCodeStartline) {
 			 this.incrementLinenumbers();
-			 System.out.println("event -> zeile: " + linenumber + " vor beobachtetem textblock eingefügt & zeilen inkrementiert");
 		 }
-		 else if (linenumber >= this.eventCodeStartline && linenumber <= this.eventCodeEndline)
-			 inside = insertEventCodeLine(linenumber, codeLine);
+		 else if (linenumber >= this.cloneCodeStartline && linenumber <= this.cloneCodeEndline)
+			 inside = insertCloneCodeLine(linenumber, codeLine);
 		 return inside;
 	 }
-	 
+	 /**
+	  * @param lineNumber and codeline
+	  * The line number of the codeline to insert 
+	  * @return boolean Ture, if change inside of a clone, otherwise false
+	  */
 	 public boolean changeLine(int linenumber, String codeLine) {
+     //A linediff change inside the clone is recognized.
 		 boolean inside = false;
-		 if (linenumber >= this.eventCodeStartline && linenumber <= this.eventCodeEndline) {
-			 inside = changeEventCodeLine(linenumber, codeLine);
+		 if (linenumber >= this.cloneCodeStartline && linenumber <= this.cloneCodeEndline) {
+			 inside = changeCloneCodeLine(linenumber, codeLine);
 		 }
 		 return inside;
 	 }
-	 
-	 private boolean changeEventCodeLine(int lineNumber, String codeLine) {
+
+
+     /**
+      * The real changing of a clone-objects code line
+      * linenNumber is the code line to change 
+      */
+	 private boolean changeCloneCodeLine(int lineNumber, String codeLine) {
 		 boolean inside = true;
-		 if (vLines != null && codeLine != null && vLines.size() > 0 && lineNumber >= 0 && this.eventCodeStartline <= lineNumber
-				 && this.eventCodeEndline >= lineNumber && vLines.size() == (this.eventCodeEndline - this.eventCodeStartline + 1)) {
+		 if (vLines != null && codeLine != null && vLines.size() > 0 && lineNumber >= 0 && this.cloneCodeStartline <= lineNumber
+				 && this.cloneCodeEndline >= lineNumber && vLines.size() == (this.cloneCodeEndline - this.cloneCodeStartline + 1)) {
 			 try {
-				 //if(!codeLine.equals("")){
-					 vLines.setElementAt(codeLine,lineNumber - this.eventCodeStartline);
-					 changeEventCode();
-					 System.out.println("event -> zeile: " + lineNumber + " in beobachtetem textblock geändert");
-				 //}
-				 /*else{
-					 deleteEventCodeLine(lineNumber);
-				 }*/
+				 vLines.setElementAt(codeLine,lineNumber - this.cloneCodeStartline);
+				 changeCloneCode();
 			 } catch (Exception e) {e.printStackTrace();}
 		 }
 		 else inside = false;
 		 return inside;
 	 }
 	 /**
-	  * 
-	  * @param lineNumber
-	  * In der alten Version die tatsächliche Zeilennummer in der Datei.
-	  * In der neuen Version der Index im Zeilenvector.
-	  * @return
-	  * In der alten Version true, falls Zeile im Codeblock gelöscht.
-	  * In der neuen Version ohne Bedeutung.
+	  * The real deleting of a clone-objects code line 
+	  * linenNumber is the index in vLines
 	  */
-	 private void deleteEventCodeLine(int lineNumber) {
-		 if (vLines != null && vLines.size() > 0 && lineNumber >= 0 && vLines.size() == (this.eventCodeEndline - this.eventCodeStartline + 1)) {
+	 private void deleteCloneCodeLine(int lineNumber) {
+		 if (vLines != null && vLines.size() > 0 && lineNumber >= 0 && vLines.size() == (this.cloneCodeEndline - this.cloneCodeStartline + 1)) {
 			 try {
 				vLines.removeElementAt(lineNumber);
 				vLinesDeleted.removeElementAt(lineNumber);
-				this.eventCodeEndline--;
-				changeEventCode();
-				System.out.println("event -> zeile: " + lineNumber + " in beobachtetem textblock gelöscht & zeilen dekrementiert");
+				this.cloneCodeEndline--;
+				changeCloneCode();
 			} catch (Exception e) {e.printStackTrace();}
 		 }
 		 if (vLines != null && vLines.size() <= 0) this.deleted = true;
 	 }
-	 private boolean insertEventCodeLine(int lineNumber, String codeLine) {
+	 /**
+	  * The real inserting of a clone-objects code line 
+	  * lineNumber is the index in vLines
+	  */
+	 
+	 private boolean insertCloneCodeLine(int lineNumber, String codeLine) {
 		 boolean inside = true;
-		 if (vLines != null && codeLine != null && vLines.size() > 0 && lineNumber >= 0 && this.eventCodeStartline <= lineNumber
-				&& this.eventCodeEndline >= lineNumber && vLines.size() == (this.eventCodeEndline - this.eventCodeStartline + 1)) {
+		 if (vLines != null && codeLine != null && vLines.size() > 0 && lineNumber >= 0 && this.cloneCodeStartline <= lineNumber
+				&& this.cloneCodeEndline >= lineNumber && vLines.size() == (this.cloneCodeEndline - this.cloneCodeStartline + 1)) {
 			try {
-				vLines.insertElementAt(codeLine,lineNumber-this.eventCodeStartline);
-				vLinesDeleted.insertElementAt(new Boolean(false),lineNumber-this.eventCodeStartline);
-				this.eventCodeEndline++;
-				changeEventCode();
-				System.out.println("event -> zeile: " + lineNumber + " in beobachtetem textblock eingefügt & zeilen inkrementiert");
+				vLines.insertElementAt(codeLine,lineNumber-this.cloneCodeStartline);
+				vLinesDeleted.insertElementAt(new Boolean(false),lineNumber-this.cloneCodeStartline);
+				this.cloneCodeEndline++;
+				changeCloneCode();
 			} catch (Exception e) {e.printStackTrace();}
 		}
 		else inside = false;
 		return inside;
 	 }
-	 private boolean markEventCodeLineAsDeleted(int lineNumber) {
+	 /**
+	  * 
+	  * @param lineNumber
+	  * @return True, if lineNumber is inside a clone.
+	  */
+	 private boolean markCloneCodeLineAsDeleted(int lineNumber) {
 		 boolean inside = true;
-		 if (vLines != null && vLines.size() > 0 && lineNumber >= 0 && this.eventCodeStartline <= lineNumber && this.eventCodeEndline >= lineNumber
-					&& vLines.size() == (this.eventCodeEndline - this.eventCodeStartline + 1)) {
+		 if (vLines != null && vLines.size() > 0 && lineNumber >= 0 && this.cloneCodeStartline <= lineNumber && this.cloneCodeEndline >= lineNumber
+					&& vLines.size() == (this.cloneCodeEndline - this.cloneCodeStartline + 1)) {
 			 try {
-				 vLinesDeleted.setElementAt(new Boolean(true),lineNumber-this.eventCodeStartline);
-				 System.out.println("event -> zeile: " + lineNumber + " in beobachtetem textblock als gelöscht markiert");
+				 vLinesDeleted.setElementAt(new Boolean(true),lineNumber-this.cloneCodeStartline);
 			 } catch (Exception e) {e.printStackTrace();}
 		 }
 		 else inside = false;
 		 return inside;
 	 }
+	 /**
+	  *  To delete the lines tah are marked as deleted lines. 
+	  *
+	  */
 	 public void clearDeletedLines() {
 		 int size = 0;
 		 if (vLines != null && vLinesDeleted != null) {
 			 size = vLinesDeleted.size();
 			 for (int index = 0;index < size;index++) {
 				 if (vLinesDeleted.elementAt(index)) {
-					 deleteEventCodeLine(index);
+					 deleteCloneCodeLine(index);
 					 size--;
 					 index--;
 				 }
 			 }
+			 if (vLines.size() == 0) deleted = true;
 		 }
 	 }
-	 public String getEventCode(){
-		 //if(eventCode != null){
-		 	return this.eventCode;
-		 //}
-		 //else return null;
-	 }
 	 
-	 private void changeEventCode(){
+	 public String getCloneCode(){
+		 	return this.cloneCode;
+	 }
+	/**
+	 * The changing of code lines in vLines.
+	 *
+	 */
+	 private void changeCloneCode(){
 		 if (vLines != null) {
 			 int lines = vLines.size();
-			 eventCode = new String();
+			 cloneCode = new String();
 			 for (int index = 0; index < lines; index++) {
-				//eventCode += vLines.get(index) + "\n";
-				 eventCode += vLines.get(index);
+				 cloneCode += vLines.get(index);
 			 }
 		 }
 	 }
